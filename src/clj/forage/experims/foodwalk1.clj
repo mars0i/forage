2445ebafa929f815614098ceac483961f9e458fc
Marshall Abrams <marshall@logical.net>
2022-02-02 22:57:27 -0600

    Small experimental mods

--- src/clj/forage/experims/foodwalk1.clj
+++ src/clj/forage/experims/foodwalk1.clj
@@ -12,11 +12,11 @@
 
 
 (def perc-radius 5)  ; distance that an animal can "see" in searching for food
-(def food-distance 50)
+(def food-distance 20)
 (def env-size 200)
 (def quadrant-size (/ env-size 2))
 (def powerlaw-scale 1) ; scale parameter of distribution
-(def maxpathlen 1000) ; max length of a path (sequence of line segments)
+(def maxpathlen 400) ; max length of a path (sequence of line segments)
 (def trunclen 100)   ; max length of any line segment
 
 ;; For Hanami/vega-lite plots, size of plot display:
