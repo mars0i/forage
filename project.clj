893ca3d8c56ff38473ffd538b35f4665c1e9a002
Marshall Abrams <marshall@logical.net>
2022-02-11 12:38:45 -0600

    Added profiles, including JVM options for speed

--- project.clj
+++ project.clj
@@ -8,7 +8,6 @@
                  [org.apache.commons/commons-math3 "3.6.1"]
                  ;[mars0i/masonclj "0.2.0"]
                  [aerial.hanami "0.17.0"]
-                 [criterium "0.4.6"]
 
 		 ;[io.github.nextjournal/clerk "0.4.316"]
 		 [io.github.nextjournal/clerk "0.5.346"]
@@ -17,32 +16,40 @@
 		;; NOTE oz MUST BE LISTED *AFTER* clerk (if clerk is present):
 		 [metasoarous/oz "2.0.0-alpha5"]
 
-                 ;[scicloj/notespace "4-alpha-21"] ; SEE REPL-OPTIONS BELOW
-                 ;[org.scicloj/tempfiles "1-alpha2"]
-                 ;[org.scicloj/kindly "1-alpha3"] ; NOTESPACE?
-
-
                  [mason "20"] ; 
                  ;; Libs that MASON wants and can be gotten from maven.org, so they don't need to be in my lib dir:
                  ;; See comment about local Maven install below.
+                 ;; DO I NEED THESE IF ONLY USING Continuous2D?
                  [org.beanshell/bsh "2.0b4"]
                  [com.lowagie/itext "1.2.3"] ; version that comes with MASON. Not in maven.org: [com.lowagie/itext "1.2"] 
                  [org.jfree/jcommon "1.0.21"]
                  [org.jfree/jfreechart "1.0.17"]
                  [javax.media/jmf "2.1.1e"]
-                 ]
+                ]
 
   :source-paths ["src/clj"]
 
-  ;; NEEDED FOR NOTESPACE:
-  ;:repl-options {:nrepl-middleware [scicloj.notespace.v4.nrepl/middleware]}
+  ;; Use (into [] (.getInputArguments (java.lang.management.ManagementFactory/getRuntimeMXBean)))
+  ;; to check JVM params.
+
+  ;; To use a profile with the repl, add "+" to the profile name, e.g:
+  ;;   lein with-profile +speedtest repl  (see https://visibletrap.blogspot.com/2015/04/start-lein-repl-with-test-profile.html)
+  :profiles {:speedtest  {:dependencies [[criterium "0.4.6"]]
+                          :jvm-opts ["-XX:TieredStopAtLevel=4" "-Xms2g"]}
+
+             :production {:jvm-opts ["-Xms2g" ; small improvement--OK to drop down to run more processes
+                                     "-XX:TieredStopAtLevel=4"]} ; 3X improvement
+
+             :notespace {:dependencies [[scicloj/notespace "4-alpha-21"]
+                                        [org.scicloj/tempfiles "1-alpha2"]
+                                        [org.scicloj/kindly "1-alpha3"]]
+                         :repl-options {:nrepl-middleware [scicloj.notespace.v4.nrepl/middleware]}}
+            }
 
   ;:repl-options {:init-ns forage.core}
   ;:global-vars {*warn-on-reflection* true}
-  ;:jvm-opts ["-Xms2g"]
   ;:aot [forage.Sim forage.GUI]
   ; SEE pasta/project.clj for other lines here I might want to include
-
 
   ;; TO INSTALL A NON-MAVENIZED JAR LOCALLY, uncomment lein-localrepo
   ;; below, run 
