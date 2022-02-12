b4de1250737b773e7cfe9d1f7e8c927c171ae132
mabrams <mabrams@uab.edu>
2022-02-12 12:26:35 -0600

    Added set-pp-width function in new file

---
+++ src/clj/utils/misc.clj
@@ -0,0 +1,8 @@
+(ns utils.misc)
+
+(defn set-pp-width 
+  "Sets width for pretty-printing with pprint and pp."
+  [cols] 
+  (alter-var-root 
+    #'clojure.pprint/*print-right-margin* 
+    (constantly cols)))
