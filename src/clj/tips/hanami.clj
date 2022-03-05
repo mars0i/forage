fe68e79049f2a54dc1590f898e54745cdf8961f5
Marshall Abrams <marshall@logical.net>
2022-03-04 23:29:10 -0600

    Removed irrelevant functions

--- src/clj/tips/hanami.clj
+++ src/clj/tips/hanami.clj
@@ -11,29 +11,4 @@
 
 (defn vegalize
   [xs]
-  (map (fn [x] {"num" x}) xs))
-
-(defn vl-levy-hist
-  [n]
-  {:data (vegalize (take n levy-nums))
-   :mark "bar"
-   :encoding {
-              :x {:bin true, :field "num"}
-              :y {:aggregate "mean"}
-             }
-  })
-
-
-
-;; NOT RIGHT
-(defn levy-hist
-  [n]
-  (hc/xform ht/bar-chart
-            :DATA (take n levy-nums)
-            :HEIGHT 300
-            :WIDTH 400
-            :X "val"
-            :Y "prob"
-            :XTITLE "Random value"
-            :YTITLE "Probability"))
-
+  (map (fn [x] {"x" x}) xs))
