373585f0a1bf2e792512f94fcc3fb834c27e4412
Marshall Abrams <marshall@logical.net>
2022-03-12 15:55:45 -0600

    Added inverse truncated Levy data

--- src/clj/forage/experims/levytounif.clj
+++ src/clj/forage/experims/levytounif.clj
@@ -1,40 +1,53 @@
 ;; Experiments with conversion of Levy distributed numbers to 
 ;; uniformly distributed numbers.
-(ns tips.levytounif
+(ns forage.experims.levytounif
   (:require [tech.viz.vega :as tv]
             [utils.random :as r]))
+
+(def truncation-max 100)
 
 (def seed (inc (r/make-seed)))
 (println "SEED:" seed)
 (def rng (r/make-well19937 seed))
 
-(def dist (r/make-powerlaw rng 1 2))
-
-(def unif-nums (repeatedly #(r/next-double rng)))
-(def levy-nums (repeatedly #(r/next-double dist)))
-(def inverse-nums (map #(.cumulativeProbability dist %) levy-nums))
-(def diffs (map - unif-nums inverse-nums)) ; interesting plot: triangular
-
-;; Note you can't just apply .cumulativeProbability to the result
-;; to get an inverse truncated distribution; the cummlative probability
-;; is different for a truncated distribution.  See PowerlawAndPareto.md
-;; and paretodistnotes.md.
 (defn truncated-nums
   "Return a sequence made from levy-nums by removing all elements that
   are >= maxval.  (Note you can also perform truncation using next-double.)"
   [xs maxval]
   (filter (partial >= maxval) xs))
 
+
+(def mu2dist (r/make-powerlaw rng 1 2))
+
+(def unif-nums (repeatedly #(r/next-double rng)))
+
+(def levy-nums (repeatedly #(r/next-double mu2dist)))
+(def inverse-levy-nums (map (partial r/cumulative mu2dist) levy-nums))
+(def levy-diffs (map - unif-nums inverse-nums)) ; interesting plot: triangular
+
+(def trunc-levy-nums (repeatedly #(r/next-double mu2dist 1 truncation-max)))
+(def inverse-trunc-levy-nums
+  (map (partial r/cumulative mu2dist 1 truncation-max) trunc-levy-nums))
+(def trunc-levy-diffs (map - unif-nums inverse-nums))
+
 (defn hist
-  "Slightly more convenient than tv/histogram, but with limitations.
+  "Slightly more convenient than tech.viz.vega/histogram, but with limitations.
   Example usage:
      (hist (take xs 1000) 100)"
   ([xs] (hist xs 50))
   ([xs bins] (tv/histogram xs "x" {:bin-count bins})))
 
 (comment
-(oz/view! (hist (take 100000 inverse-nums)))
-(oz/view! (hist (take 100000 unif-nums)))
-(oz/view! (hist (take 100000 (truncated-levy 25))))
+  (require '[oz.core :as oz])
+
+  (oz/view! (hist (take 100000 unif-nums)))
+
+  (oz/view! (hist (take 100000 levy-nums)))
+  (oz/view! (hist (take 100000 inverse-levy-nums)))
+  (oz/view! (hist (take 100000 levy-diffs)))
+
+  (oz/view! (hist (take 100000 trunc-levy-nums)))
+  (oz/view! (hist (take 100000 inverse-trunc-levy-nums)))
+  (oz/view! (hist (take 100000 trunc-levy-diffs)))
 )
 
