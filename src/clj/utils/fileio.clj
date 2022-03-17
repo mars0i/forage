0a74622cf6f80234a507e6f9ed2c40ca8585583e
Marshall Abrams <marshall@logical.net>
2022-03-17 14:03:45 -0500

    Added spit-csv-with-header

--- src/clj/utils/fileio.clj
+++ src/clj/utils/fileio.clj
@@ -5,9 +5,17 @@
 ;;;;;;;;;;;;;;;;;;
 
 (defn spit-csv
-  "Given a sequence of sequences of data, opens a file and writes to it
-  using write-csv.  Options are those that can be passed to 
-  clojure.java.io/writer."
+  "Given a sequence of sequences of data in rows, opens a file and
+  writes to it using write-csv.  options are those that can be passed
+  to clojure.java.io/writer."
   [filename rows & options]
    (with-open [w (apply io/writer filename options)]
      (csv/write-csv w rows)))
+
+(defn spit-csv-with-header
+  "Given a sequence of sequences of data in rows, opens a file and
+  writes to it using write-csv after prepending the header row.  
+  options are those that can be passed to clojure.java.io/writer."
+  [header filename rows & options]
+  (let [header+rows (cons header rows)]
+    (apply spit-csv filename header+rows options)))
