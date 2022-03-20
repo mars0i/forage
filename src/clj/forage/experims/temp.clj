95148ce179009e7c0d7b08ca6cc24203b018ce97
Marshall Abrams <marshall@logical.net>
2022-03-19 23:52:45 -0500

    Current temporary experimental code

---
+++ src/clj/forage/experims/temp.clj
@@ -0,0 +1,29 @@
+;; File for misc experiments.  Think of it as a repl.
+(ns forage.experims.temp
+  (:use [forage.experims.manywalks1])
+  (:require [forage.viz.hanami :as h]
+            [forage.walks :as w]
+            [forage.food :as f]
+            [forage.mason.foodspot :as mf]
+            [utils.math :as m]
+            [utils.random :as r]))
+
+(comment 
+  (def lws (repeatedly levy-fn))
+  (def sws (map (fn [t] (straight-fn (* (/ t 200) m/pi))) (range 201)))
+
+  (def sws-stats (map length-when-found sws))
+
+  (def yo (second (first lws)))
+  (w/stops-path-len yo)
+
+
+
+)
+
+(comment
+  (require '[oz.core :as oz])
+  (oz/start-server!)
+  (def splot (make-gridwalk-plot env-size plot-dim food-distance display-radius sws))
+  (oz/view! splot)
+)
