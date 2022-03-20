78e268c375beaf67a54055da8ced0a0b9b8d6fe9
Marshall Abrams <marshall@logical.net>
2022-03-20 12:59:07 -0500

    Misc experiments

--- src/clj/forage/experims/temp.clj
+++ src/clj/forage/experims/temp.clj
@@ -8,17 +8,20 @@
             [utils.math :as m]
             [utils.random :as r]))
 
+
+;; Should work since we `used`d manywalks1 above:
+(r/next-double dist)
+
 (comment 
   (def lws (repeatedly levy-fn))
   (def sws (map (fn [t] (straight-fn (* (/ t 200) m/pi))) (range 201)))
-
   (def sws-stats (map length-when-found sws))
+
+  (first sws)
+  (map count (first lws))
 
   (def yo (second (first lws)))
   (w/stops-path-len yo)
-
-
-
 )
 
 (comment
