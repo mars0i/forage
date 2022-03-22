e3af5a90741431e0bb882bd287531386a692fb25
Marshall Abrams <marshall@logical.net>
2022-03-21 21:30:25 -0500

    Moved temporary mucking-about file

---
+++ src/clj/scratch.clj
@@ -0,0 +1,32 @@
+;; File for misc experiments.  Think of it as a repl.
+(ns scratch
+  (:use [forage.experims.manywalks1])
+  (:require [forage.viz.hanami :as h]
+            [forage.walks :as w]
+            [forage.food :as f]
+            [forage.mason.foodspot :as mf]
+            [utils.math :as m]
+            [utils.random :as r]))
+
+
+;; Should work since we `used`d manywalks1 above:
+(r/next-double dist)
+
+(comment 
+  (def lws (repeatedly levy-fn))
+  (def sws (map (fn [t] (straight-fn (* (/ t 200) m/pi))) (range 201)))
+  (def sws-stats (map length-when-found sws))
+
+  (first sws)
+  (map count (first lws))
+
+  (def yo (second (first lws)))
+  (w/stops-path-len yo)
+)
+
+(comment
+  (require '[oz.core :as oz])
+  (oz/start-server!)
+  (def splot (make-gridwalk-plot env-size plot-dim food-distance display-radius sws))
+  (oz/view! splot)
+)
