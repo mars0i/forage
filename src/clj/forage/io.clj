7aaa9ef19bc5c1fd09af43a765cd05abb2411315
Marshall Abrams <marshall@logical.net>
2022-03-29 12:48:58 -0500

    Moved spit-csv from utils/io.clj to forage/io.clj, deleted the former

--- src/clj/forage/io.clj
+++ src/clj/forage/io.clj
@@ -1,5 +1,8 @@
 (ns forage.io
-  (:require [utils.io :as uio]))
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
+;;;;;;;;;;;;;;;;;;
 
 ;; to write to file:
 ;; - seed at start of runs
@@ -15,18 +18,30 @@
 ;;    or points) of walk up to food. Or this can be gotten later if needed by 
 ;;    re-running if necessary.
 
-(defn write-param-names
-  [filename param-names]
-  (uio/spit-csv filename
-                (concat ["seed"] param-names "found" "segments")))
+(defn spit-csv
+  "Given a sequence of sequences of data in rows, opens a file and
+  writes to it using write-csv.  options are those that can be passed
+  to clojure.java.io/writer."
+  [filename rows & options]
+   (with-open [w (apply io/writer filename options)]
+     (csv/write-csv w rows)))
 
-(defn write-set-of-runs
-  [filename seed parameter-seq found-count segment-count]
-  )
-
-(defn add-row-from-runs
+(defn append-row-from-runs
+  "Given a value for seed, a sequence of parameters, a count of found
+  foodspots in a collection of runs, and the total number of segments in
+  all of the full walks (before truncation due to finding food) in the
+  collection, appends a new row to existing rows and returns the result.
+  Apart from params being a sequence, there are no restrictions on content,
+  so this can be used to write a row of labels as well."
   ([seed params found-count total-segment-count]
-   (add-row-from-runs [] seed params found-count total-segment-count))
+   (append-row-from-runs [] seed params found-count total-segment-count))
   ([prev-rows seed params found-count total-segment-count]
    (conj prev-rows (concat [seed] params [found-count total-segment-count]))))
 
+(defn append-labels
+  "Appends a new row with standard labels in addition to param-names.  Uses 
+  append-row-from-runs."
+  ([param-names]
+   (append-row-from-runs "seed" param-names "found" "segments"))
+  ([prev-rows param-names] 
+   (append-row-from-runs prev-rows "seed" param-names "found" "segments")))
