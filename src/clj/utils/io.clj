e1ecac70ed2e48c5248fc78eba3ca1eebed8bf9f
Marshall Abrams <marshall@logical.net>
2022-03-29 12:35:34 -0500

    Removed spit-csv-with-headers

--- src/clj/utils/io.clj
+++ src/clj/utils/io.clj
@@ -11,11 +11,3 @@
   [filename rows & options]
    (with-open [w (apply io/writer filename options)]
      (csv/write-csv w rows)))
-
-(defn spit-csv-with-header
-  "Given a sequence of sequences of data in rows, opens a file and
-  writes to it using write-csv after prepending the header row.  
-  options are those that can be passed to clojure.java.io/writer."
-  [header filename rows & options]
-  (let [header+rows (cons header rows)]
-    (apply spit-csv filename header+rows options)))
