37ae63bca0c1634cd6f5982979ed2ad8ffb16ab2
mabrams <mabrams@uab.edu>
2022-03-29 23:35:02 -0500

    Misc bugfixes and simplifications

--- src/clj/forage/experiment/grid1.clj
+++ src/clj/forage/experiment/grid1.clj
@@ -10,18 +10,26 @@
 ;(def seed 1649988521705)
 (println "SEED:" seed)
 
+
+(def env-size 20000) ; full width of env
+(def half-size (/ env-size 2))
+
+;; For straight walk should be <= env-size/2 because
+;; will start at (env-size, env-size):
+(def maxpathlen half-size)
+
 (def params (sorted-map
               :perc-radius 1  ; distance that an animal can "see" in searching for food
               :food-distance 200
-              :env-size 20000 ; full width of env
-              :half-size (/ env-size 2)
+              :env-size env-size
+              :half-size half-size
               :init-loc [half-size half-size]
               :init-dir 0
-              :maxpathlen half-size ; for straight walk should be <= env-size/2 because will start at (env-size, env-size)
+              :maxpathlen maxpathlen
               :trunclen maxpathlen   ; max length of any line segment
               :default-direction 0
               :look-eps 0.1 ; increment within line segments for food check
-             )
+             ))
 ;:powerlaw-scale 1 ; scale parameter of distribution
 ;:powerlaw-exponent 2 ; must be > 1; 2 supposed to be optimal sparse targets
 
@@ -29,6 +37,44 @@
 (def scales [1 2 4 8])
 
 
+
+
+(defn levy-experiments
+  [num-walks seed parms scales exponents]
+  (let [rng (r/make-well19937 seed)
+        env (mf/make-env (parms :food-distance)
+                         (parms :env-size)
+                         (f/centerless-rectangular-grid (parms :food-distance)
+                                                        (parms :env-size)
+                                                        (parms :env-size)))
+        look-fn (partial mf/perc-foodspots-exactly env (parms :perc-radius))
+        data 'fixme] ;; initialize with header row here
+    (doseq [scale scales
+            exponent exponents]
+      (let [sim-fn #(w/levy-foodwalk look-fn (parms :look-eps) (parms :init-loc)
+                                     (parms :maxpathlen) (parms :init-dir)
+                                     (parms :trunclen) rng
+                                     scale exponent)
+            foodwalks+ (doall (repeatedly num-walks sim-fn))
+            found (w/count-found-foodspots foodwalks+)
+            segments (w/count-segments 2 foodwalks+)]
+        ; append data row to datap
+        ))
+  ; write data to file
+  ))
+
+
+;      (let [sim-fn #(w/levy-foodwalk look-fn (parms :look-eps) (parms :init-loc)
+;                                     (parms :maxpathlen) (parms :init-dir)
+;                                     (parms :trunclen) rng
+;                                     scale exponent)
+;            foodwalks+ (doall (repeatedly num-walks sim-fn))
+
+
+;;;;;;;;;;;;;;;;;;;
+;; OLD STUFF
+
+(comment
 ;; FIXME REMOVE THIS WHEN READY
 (defn levy-fw 
   "Generates Levy foodwalk data from a Levy walk using uniformly distributed
@@ -49,34 +95,14 @@
 (defn levy-fws
   [rng scale exponent]
   (repeatedly #(levy-fw rng scale exponent)))
+)
 
 
-(defn levy-experiments
-  [num-walks seed params scales exponents]
-  (let [rng (r/make-well19937 seed)
-        env (mf/make-env (params :food-distance)
-                         (params :env-size)
-                         (f/centerless-rectangular-grid (params :food-distance)
-                                                        (params :env-size)
-                                                        (params :env-size)))
-        look-fn (partial mf/perc-foodspots-exactly env (params :perc-radius))
-        data 'fixme] ;; initialize with header row here
-    (doseq [scale scales
-            exponent exponents]
-      (let [sim-fn #(w/levy-foodwalk look-fn (params :look-eps) (params :init-loc)
-                                     (params :maxpathlen) (params :init-dir)
-                                     (params :trunclen) rng
-                                     scale exponent)
-            foodwalks+ (doall (repeatedly num-walks sim-fn))
-            found (w/count-found-foodspots foodwalks+)
-            segments (reduce + (map #(dec (count %))
-                                    (third foodwalks+)))]
-        ; append data row to data
-        ))
-  ; write data to file
-  ))
+(comment
 
-
+;; Divide quadrant into n directions in radians:
+(def quadrant-100-directions  (doall (map #(* (/ % 100) (/ m/pi 2)) (range 100)))) 
+(def quadrant-200-directions  (doall (map #(* (/ % 200) (/ m/pi 2)) (range 200)))) 
 
 ;; Could be defined with partial, but this way there's a docstring ; parameter to *this* function
 (defn straight-fw 
@@ -91,30 +117,8 @@
                    look-eps init-loc maxpathlen                            ; also above
                    init-dir))
 
-;; Divide quadrant into n directions in radians:
-(def quadrant-100-directions  (doall (map #(* (/ % 100) (/ m/pi 2)) (range 100)))) 
-(def quadrant-200-directions  (doall (map #(* (/ % 200) (/ m/pi 2)) (range 200)))) 
-
 (defn straight-fws
   [init-dirs]
   (map straight-fw init-dirs))
 
-(defn foodspot-coords-if-found
-  "Given a triple returned by walks/levy-foodwalk or walks/straight-foodwalk,
-  returns the coordinates of the first found foodspot, or nil if there are none.
-  Note that this uses foodspot/foodspot-coords* , which shouldn't be used in
-  production code."
-  [[found-foodspot-seq _ _]]
-  (if found-foodspot-seq
-    (mf/foodspot-coords* (first found-foodspot-seq))
-    nil))
-
-(defn foodspot-coords-if-found-seq
-  "Maps foodspot-coords-if-found over each element in a sequence of returned
-  foodwalk triples and returns the resulting (lazy) sequence."
-  [foodwalks]
-  (map foodspot-coords-if-found foodwalks))
-
-(comment
-
-        )
+)
