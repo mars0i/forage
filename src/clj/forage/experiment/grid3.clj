b9f1d8ec387de8c4decb439f384f1e6fcffdf781
mabrams <mabrams@uab.edu>
2022-04-04 17:50:18 -0500

    New experiment runs with random initial direction

---
+++ src/clj/forage/experiment/grid3.clj
@@ -0,0 +1,48 @@
+(ns forage.experiment.grid3
+  (:require
+   [forage.run :as fr]
+   [utils.random :as r]
+   [utils.math :as m]))
+
+(def seed (inc (r/make-seed)))
+(println "SEED:" seed)
+
+(def exponents [1.001  1.2  1.3 1.4 1.5 1.6 1.7 1.8 1.9 2  2.2  2.5  3])
+(def walks-per-combo 100) ; only for levy-experiments (straight-experiments isn't random)
+
+;; TESTING
+;(def exponents [1.001 1.5 2 2.5 3])
+;(def walks-per-combo 100)
+(def exponents [1.001 2])
+(def walks-per-combo 2)
+
+
+;; Fixed parameters for all runs
+(def half-size 10000) ; half the full width of the env
+(def params (sorted-map ; sort so labels match values
+              :powerlaw-min      1
+              :perc-radius       1  ; distance that an animal can "see" in searching for food
+              :food-distance     200
+              :env-size          (* 2 half-size)
+              :init-loc          [half-size half-size] ; i.e. center of env
+              :maxpathlen        half-size  ; for straight walks, don't go too far
+              :trunclen          half-size  ; max length of any line segment
+              :look-eps          0.1  ; increment within segments for food check
+              :max-frac          0.25 ; proportion of pi to use as maximum direction (0 is min) ; ignored if num-dirs is falsey
+              :num-dirs          50 ; split range this many times + 1 (includes range max); nil for random
+             ))
+
+(comment
+  ;; Parameters for testing:
+  (require '[forage.run :as fr])
+  ;(def params (assoc params :num-dirs 20))
+  (def exponents [1.001 2])
+  (def walks-per-combo 2)
+
+  (use 'clojure.pprint)
+  (time (def data (fr/levy-experiments fr/default-file-prefix seed params exponents walks-per-combo)))
+  (time (def data (fr/straight-experiments fr/default-file-prefix params)))
+  (pprint data)
+  (pprint fw+)
+)
+
