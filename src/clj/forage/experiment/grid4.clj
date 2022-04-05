e77f4e8931fed720193c4ddb88968fc6a18b9d38
mabrams <mabrams@uab.edu>
2022-04-04 19:55:51 -0500

    New experiment like grid3 but bigger env larger distance between foodspots

---
+++ src/clj/forage/experiment/grid4.clj
@@ -0,0 +1,52 @@
+
+(ns forage.experiment.grid3
+  (:require
+   [forage.run :as fr]
+   [utils.random :as r]
+   [utils.math :as m]))
+
+(def seed (inc (r/make-seed)))
+(println "SEED:" seed)
+
+(def exponents [1.001 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2 2.1 2.2 2.4 2.5  3])
+(def walks-per-combo 5000) ; only for levy-experiments (straight-experiments isn't random)
+;; Note Excel might have a 16K columns max
+
+;(/ (/ (* (* 16 5000) 0.1) 60) 60)
+
+;; TESTING
+;(def exponents [1.001 1.5 2 2.5 3])
+;(def walks-per-combo 100)
+;(def exponents [1.001 2])
+;(def walks-per-combo 2)
+
+
+;; Fixed parameters for all runs
+(def half-size 100000) ; half the full width of the env
+(def params (sorted-map ; sort so labels match values
+              :powerlaw-min      1
+              :perc-radius       1  ; distance that an animal can "see" in searching for food
+              :food-distance     400
+              :env-size          (* 2 half-size)
+              :init-loc          [half-size half-size] ; i.e. center of env
+              :maxpathlen        half-size  ; for straight walks, don't go too far
+              :trunclen          half-size  ; max length of any line segment
+              :look-eps          0.1  ; increment within segments for food check
+              :max-frac          0.25 ; proportion of pi to use as maximum direction (0 is min) ; ignored if num-dirs is falsey
+              :num-dirs          nil ; split range this many times + 1 (includes range max); nil for random
+             ))
+
+(comment
+  ;; Parameters for testing:
+  (require '[forage.run :as fr])
+  ;(def params (assoc params :num-dirs 20))
+  (def exponents [1.001 2])
+  (def walks-per-combo 2)
+
+  (use 'clojure.pprint)
+  (time (def data (fr/levy-experiments fr/default-file-prefix seed params exponents walks-per-combo)))
+  (time (def data (fr/straight-experiments fr/default-file-prefix params)))
+  (pprint data)
+  (pprint fw+)
+)
+
