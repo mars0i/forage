9f027949e61db468cb518dc524a91dc4653fb37b
mabrams <mabrams@uab.edu>
2022-04-05 23:54:43 -0500

    New experiments using Fournier envs

--- src/clj/forage/explore/grid5.clj
+++ src/clj/forage/explore/grid5.clj
@@ -2,16 +2,23 @@
   (:require [forage.run :as fr]
             [forage.food :as f]
             [forage.mason.foodspot :as mf]
+            [forage.viz.hanami :as h]
             [utils.random :as r]
             [utils.math :as m]))
 
-;(def seed (inc (r/make-seed)))
-;(println "SEED:" seed)
+
+(def seed (r/make-seed))
+(println seed)
+(def rng (r/make-well19937 seed))
+
 
 (def exponents [2])
+;(def exponent 2)
 (def walks-per-combo 1)
-(def half-size 50000) ; half the full width of the env
-(def food-distance 400)
+;(def half-size 50000) ; half the full width of the env
+(def half-size 10000) ; half the full width of the env
+;(def food-distance 500)
+(def food-distance 2000)
 (def params (sorted-map ; sort so labels match values
               :food-distance     food-distance
               :perc-radius       1  ; distance that an animal can "see" in searching for food
@@ -19,21 +26,46 @@
               :env-size          (* 2 half-size)
               :env-discretization food-distance
               :init-loc          [half-size half-size] ; i.e. center of env
-              :maxpathlen        half-size  ; for straight walks, don't go too far
-              :trunclen          half-size  ; max length of any line segment
+              :maxpathlen        (* 4 half-size)  ; for straight walks, don't go too far
+              :trunclen          (* 4 half-size ) ; max length of any line segment
               :look-eps          0.1  ; increment within segments for food check
               :max-frac          0.25 ; proportion of pi to use as maximum direction (0 is min) ; ignored if num-dirs is falsey
               :num-dirs          nil ; split range this many times + 1 (includes range max); nil for random
              ))
 
 (def grid-env (mf/make-env (params :env-discretization)
-                      (params :env-size)
-                      (f/centerless-rectangular-grid (params :food-distance)
-                                                     (params :env-size)
-                                                     (params :env-size))))
+                           (params :env-size)
+                           (f/centerless-rectangular-grid (params :food-distance)
+                                                          (params :env-size)
+                                                          (params :env-size))))
 
+(def grid-look-fn (partial mf/perc-foodspots-exactly grid-env (params :perc-radius)))
+
+(def fourn-env (mf/make-env (params :env-discretization)
+                            (params :env-size)
+                            (f/fournierize (mf/all-foodspot-coords grid-env)
+                                           1000 0.2 3)))
+
+(def fourn-look-fn (partial mf/perc-foodspots-exactly fourn-env (params :perc-radius)))
 
 (comment
+  (do
+    (def seed (r/make-seed))
+    (def rng (r/make-well19937 seed))
+    (def ffw+ (fr/levy-run rng fourn-look-fn nil params 2))
+    (oz/view! (h/vega-envwalk-plot fourn-env 1100 20 [ffw+]))
+   )
+
+  (def ffw+ (fr/levy-run (r/make-well19937) fourn-look-fn nil params 3))
+  (oz/view! (h/vega-envwalk-plot fourn-env 1100 20 [ffw+]))
+
+  (def gfw+ (fr/levy-run rng grid-look-fn nil params 2))
+  (def gfw+ (fr/levy-run (r/make-well19937) grid-look-fn nil params 2))
+  (oz/view! (h/vega-envwalk-plot grid-env 1100 50 [gfw+]))
+
+  (require '[oz.core :as oz])
+  (oz/start-server!)
+
   (time (fr/levy-experiments fr/default-file-prefix grid-env (r/make-seed) params exponents walks-per-combo))
 )
 
