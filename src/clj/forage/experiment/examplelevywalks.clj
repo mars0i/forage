d2e49c75b0947ce32194d9103a64afec8c10992a
mabrams <mabrams@uab.edu>
2022-04-07 13:34:55 -0500

    Working generation of one plot with three mus

--- src/clj/forage/experiment/examplelevywalks.clj
+++ src/clj/forage/experiment/examplelevywalks.clj
@@ -1,9 +1,98 @@
 (ns forage.experiment.examplelevywalks
-  (:require [forage.run :as fr]
+  (:require [forage.walks :as w]
             [utils.random :as r]
             [forage.viz.hanami :as h]
             [oz.core :as oz]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; HANAMI SPACE PARAMETERS:
+
+(def env-size 5000)
+(def half-size (/ env-size 2))
+(def plot-dim 700)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DATA PARAMETERS:
+
+;; $\mu \approx 1$ is is a "ballistic" Levy walk.
+;; $\mu = 2$ is the theoretical optimum for searches.
+;; $\mu = 3$ is a Brownian walk.
+;(def mus [1.000001 2 2.2 3])  ; mu near 1: decimal digits have big effect on speed
+(def mus [1.01 2 3])  ; mu near 1: decimal digits have big effect on speed
+;(def mus [2])
+;(def scales [1])
+;(def scales [1 8])
+;(def scales [1 10 100])
+;(def scales [1 100 1000])
+;(def scale-mus (for [scale scales, mu mus] [scale mu]))
+
+(def maxpathlen 5000)
+(def trunclen 5000)
+(def perceptual-radius 20) 
+(def food-distance 200)
+
+(def powerlaw-scale 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PRNGS and DISTS:
+
+(def seed (inc (r/make-seed)))
+(def rng (r/make-well19937 seed))
+
+(def dists (map (fn [mu] (r/make-powerlaw rng 1 mu)) mus))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; RANDOM WALK DATA GENERATION:
+
+;; Add this to the embedded fn below to constrain all seqs to start in the
+;; same direction: (w/subst-init-dir init-dir ...)
+
+;; Infinite seqs of direction, length step vectors.  Used by two defs below.
+(def inf-step-seqs (map
+                     (fn [dist]
+                         (repeatedly
+                           (w/step-vector-fn rng dist 1 trunclen))) dists))
+
+(def step-seqs (map (partial w/vecs-upto-len maxpathlen) inf-step-seqs))
+
+
+;; Infinite sequences of steps that result from the step vectors
+(def stop-seqs (map (fn [step-seq]
+                      (w/walk-stops [half-size half-size] step-seq))
+                    step-seqs))
+
+;; Infinite sequences of stops labeled for Hanami/Vega-Lite:
+(def vl-stop-seqs (map (fn [mu stop-seq]
+                           (h/add-walk-labels (str "μ=" mu) stop-seq))
+                       mus stop-seqs))
+
+;; Number of steps in each path:
+(def n-steps (map count step-seqs))
+
+;; Construct walk data for Hanami/Vega-Lite:
+(def walks (doall (apply concat (map take n-steps vl-stop-seqs))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; HANAMI/VEGA-LITE
+
+(def gridwalk-plot (h/vega-gridwalk-plot
+                     perceptual-radius maxpathlen powerlaw-scale n-steps
+                     (h/vega-linegrid-plot env-size plot-dim food-distance) ; kludge to force uniform scaling and dims
+                     (h/vega-walk-plot plot-dim walks)))
+
+
+;; Now view gridwalk as vega-lite, e.g. with
+(comment
+  ;(require '[oz.core :as oz])
+  (oz/start-server!)
+  (oz/view! gridwalk-plot)
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; OLD CODE
+
+(comment
 (def half-size 1000)
 (def food-distance 100)
 (def params {:food-distance       food-distance
@@ -54,4 +143,4 @@
   (oz/view! mu2plot)
   (oz/view! mu1plot)
 )
-
+)
