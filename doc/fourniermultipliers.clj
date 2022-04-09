ab1e4e2d8fa13b1d4c91d62fb579ed2b90333872
Marshall Abrams <marshall@logical.net>
2022-04-09 17:04:25 -0500

    copied code exploring Fournier multipliers to doc

---
+++ doc/fourniermultipliers.clj
@@ -0,0 +1,47 @@
+
+
+(comment
+  ;; Effects of different Fournier multipliers:
+  init-food
+
+  ;; 3 levels
+  (* init-food 0.01)
+  (* init-food 0.01 0.01)
+  (* init-food 0.01 0.01 0.01)
+
+  ;; 3 levels
+  (* init-food 0.05)
+  (* init-food 0.05 0.05)
+  (* init-food 0.05 0.05 0.05)
+
+  ;; 4 levels
+  (* init-food 0.1)
+  (* init-food 0.1 0.1)
+  (* init-food 0.1 0.1 0.1)
+  (* init-food 0.1 0.1 0.1 0.1)
+
+  ;; 4 levels
+  (* init-food 0.15)
+  (* init-food 0.15 0.15)
+  (* init-food 0.15 0.15 0.15)
+  (* init-food 0.15 0.15 0.15 0.15)
+
+  ;; 4 levels
+  (* init-food 0.2)
+  (* init-food 0.2 0.2)
+  (* init-food 0.2 0.2 0.2)
+  (* init-food 0.2 0.2 0.2 0.2)
+
+  ;; 5 levels
+  (* init-food 0.25)
+  (* init-food 0.25 0.25)
+  (* init-food 0.25 0.25 0.25)
+  (* init-food 0.25 0.25 0.25 0.25)
+  (* init-food 0.25 0.25 0.25 0.25 0.25)
+
+  ;; 4 levels
+  (* init-food 0.5)
+  (* init-food 0.5 0.5)
+  (* init-food 0.5 0.5 0.5)
+  (* init-food 0.5 0.5 0.5 0.5)
+)
