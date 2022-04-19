0c2611745a3e32081ab669ef5b431c37f3956ca2
Marshall Abrams <marshall@logical.net>
2022-04-19 13:19:13 -0500

    Moved fournierize to new file utils/fractal.clj

--- src/clj/forage/food.clj
+++ src/clj/forage/food.clj
@@ -2,7 +2,8 @@
 ;; (rectangular) spatial coordinates.
 ;; (Code s/b independent of MASON and plot libs (e.g. Hanami, Vega-Lite).)
 (ns forage.food
-    (:require [utils.math :as m]))
+  (:require [utils.math :as m]
+            [utils.fractal :as uf]))
 
 
 (declare x-zero? y-zero? either-zero? both-zero?)
@@ -55,34 +56,16 @@
                                [x y]))]
      (concat ne-pairs sw-pairs se-pairs nw-pairs))))
 
-(defn fournier-children
-  "Given a coordinate pair, return four coordinate pairs that are
-  shifted by offset up, down, left, and right from the original point."
-  [offset [x y]]
-  [[(+ x offset) y] [x (+ y offset)]
-   [(- x offset) y] [x (- y offset)]])
-
-;; FIXME ?  I'm using huge values for sep.  Maybe this is the wrong
-;; approach.  cf. Mandelbrot's way of constructing Cantor dusts, including
-;; the infinitely large ones.
-;;
-;; Inspired by Mandelbrot's description of Fournier d'Albe's model universe.
-;; See Mandelbrot's _The Fractal Geometry of Nature_ pp. 86-87 and 95-96,
-;; or one of its predecessor books.
 (defn fournierize
-  "Given a sequence of coordinate pairs (points), returns a sequence containing
+  "DEPRECATED: Use utils.fractal/fournierize.
+  Given a sequence of coordinate pairs (points), returns a sequence containing
   those points and \"fournier children\", i.e. points that are (* sep multiplier)
   up, down, left, and to the right of each original point.  Then iterates,
   performing the same operation on all of the points at a smaller scale, levels
   times.  multiplier should be < 1.  (Note that the number of points is increased
   exponentially, multiplying by 5 each time.)"
   [points sep multiplier levels]
-  (loop [pts points, offset sep, iters levels]
-    (if (<= iters 0)
-      pts
-      (let [new-offset (* offset multiplier)
-            new-pts (mapcat (partial fournier-children new-offset) pts)]
-        (recur (into pts new-pts) new-offset (dec iters))))))
+  (uf/fournierize points sep multiplier levels))
 
 (comment
   (def points [[0 0] [0 100] [0 -100] [100 0] [-100 0]])
