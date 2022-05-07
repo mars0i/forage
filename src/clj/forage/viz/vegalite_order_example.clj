5d395668a3b116067dc3362906084d452d2eb7d8
mabrams <mabrams@uab.edu>
2022-05-07 09:08:44 -0500

    Added comment

--- src/clj/forage/viz/vegalite_order_example.clj
+++ src/clj/forage/viz/vegalite_order_example.clj
@@ -6,7 +6,7 @@
   {:$schema "https://vega.github.io/schema/vega-lite/v5.json"
    :data {:url "https://vega.github.io/vega-lite/data/driving.json"}
    :encoding {:order {:field "year"}
-   ;:encoding {:order {:field "year" :type "ordinal"}
+   ;:encoding {:order {:field "year" :type "ordinal"} ; fixes warning 
               :x {:field "miles" :scale {:zero false} :type "quantitative"}
               :y {:field "gas" :scale {:zero false} :type "quantitative"}}
    :mark {:point true :type "line"}})
