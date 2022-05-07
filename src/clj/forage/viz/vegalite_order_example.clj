6196a20584e2266df9eeb2a10c6fc812dcb934ca
mabrams <mabrams@uab.edu>
2022-05-07 00:13:28 -0500

    Added Vega-Lite example illustration of :order

---
+++ src/clj/forage/viz/vegalite_order_example.clj
@@ -0,0 +1,12 @@
+(ns forage.viz.vegalite-order-example)
+
+;; From https://behrica.github.io/vl-galery/convert, based on
+;; https://vega.github.io/vega-lite/examples/connected_scatterplot.html
+(def connected-scatterplot
+  {:$schema "https://vega.github.io/schema/vega-lite/v5.json"
+   :data {:url "https://vega.github.io/vega-lite/data/driving.json"}
+   :encoding {:order {:field "year"}
+   ;:encoding {:order {:field "year" :type "ordinal"}
+              :x {:field "miles" :scale {:zero false} :type "quantitative"}
+              :y {:field "gas" :scale {:zero false} :type "quantitative"}}
+   :mark {:point true :type "line"}})
