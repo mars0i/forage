45f5c3274374de00c5bdd96431a3f02727fceec9
mabrams <mabrams@uab.edu>
2022-05-14 12:11:54 -0500

    New params, notes re exploration of env config

--- src/clj/forage/experiment/grid5.clj
+++ src/clj/forage/experiment/grid5.clj
@@ -7,8 +7,32 @@
             [utils.math :as m]))
 
 
+;; with notes about preliminary results:
 (def half-size 5000) ; half the full width of the env
-(def init-food 200)
+(def init-food 200) ; about 18% success straight, 30% success mu=2, 75% success mu=1.5 since food so dense and regular
+
+(def half-size 5000) ; half the full width of the env
+(def init-food 400)
+   ; straight: 6/101
+   ;---
+   ; mu=3.0: 21/360
+   ; mu=3.0: 30/360
+   ; mu=3.0: 26/360
+   ;---
+   ; mu=2.5: 49/360
+   ; mu=2.5: 44/360
+   ;---
+   ; mu=2.0: 69/360
+   ; mu=2.0: 77/360
+   ;---
+   ; mu=1.5: 81/360
+   ; mu=1.5: 89/360
+   ;---
+   ; mu=1.001: 84/360
+   ; mu=1.001: 81/360
+
+(def half-size 5000) ; half the full width of the env
+(def init-food 1000)
 
 (def params (sorted-map ; sort so labels match values
              :food-distance       init-food ; ignored??
@@ -41,14 +65,21 @@
   (def seed (r/make-seed))
   (def rng (r/make-well19937 seed))
 
+  (def mu 3.0)
+  (def mu 2.5)
   (def mu 2.0)
-  (def fw (fr/levy-run rng look-fn nil params mu))
-  (time (def fws (doall (repeatedly 18 #(fr/levy-run rng look-fn nil params mu)))))
+  (def mu 1.5)
+  (def mu 1.1001)
+  ;(def fw (fr/levy-run rng look-fn nil params mu))
+  ;(time (def fws (doall (repeatedly 18 #(fr/levy-run rng look-fn nil params mu)))))
+  ;(time (def fws (doall (repeatedly 24 #(fr/levy-run rng look-fn nil params mu)))))
+  (time (def fws (doall (repeatedly 360 #(fr/levy-run rng look-fn nil params mu)))))
   (count (filter first fws))
   (def sorted-fws (w/sort-foodwalks fws))
   (fr/write-foodwalk-plots 
            (str (System/getenv "HOME") "/docs/src/data.foraging/forage/yo_mu" mu)
-           :svg seed env 800 9 3 50 mu params sorted-fws)
+           :svg seed env 800 12 3 50 mu params sorted-fws)
+           ;:svg seed env 800 9 3 50 mu params sorted-fws)
 
   (fr/straight-experiments 
            (str (System/getenv "HOME") "/docs/src/data.foraging/forage/yostraight")
