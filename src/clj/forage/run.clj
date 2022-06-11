4595a22ba72786b353072e4a1cbb75613270064d
Marshall Abrams <marshall@logical.net>
2022-06-11 14:15:08 -0500

    Removed some old debug printlns

--- src/clj/forage/run.clj
+++ src/clj/forage/run.clj
@@ -147,13 +147,10 @@
   total number of segments, total number of foodspots found, and a sequence
   of lengths of paths until foodspots were found.  n-walks must be >= 0."
   [sim-fn n-walks]
-  ;(println) ; DEBUG
   (loop [n n-walks, segments 0, found 0, lengths nil]
     (if (zero? n)
       [segments found lengths]
-      (let [;_ (print (- n-walks n) "") ; DEBUG
-            ;_ (flush) ; DEBUG
-            fw (sim-fn)]
+      (let [fw (sim-fn)]
         (recur (dec n)
                (+ segments (w/count-segments-until-found fw))
                (+ found (count (first fw)))
