4ee379411781757917b72bfaf652cca519ee7097
Marshall Abrams <marshall@logical.net>
2022-07-30 19:30:37 -0500

    Added example from Vega-Lite site

--- src/clj/scratch.clj
+++ src/clj/scratch.clj
@@ -46,4 +46,20 @@
   (first stops-)
   (cp/three-plots 500 200 stops)
 
+
+  ;; https://vega.github.io/vega-lite/docs/size.html#example
+  (def example {:width 300,
+                ;:autosize {:type "fit",
+                ;           :contains "padding"
+                ;           },
+                :data {:values [{:a "A", :b 28}, {:a "B", :b 55}, {:a "C", :b 43},
+                                {:a "D", :b 91}, {:a "E", :b 81}, {:a "F", :b 53},
+                                {:a "G", :b 19}, {:a "H", :b 87}, {:a "I", :b 52}]},
+                :mark "bar",
+                :encoding {
+                           :x {:field "a", :type "ordinal"},
+                           :y {:field "b", :type "quantitative"}}})
+
+  (oz/start-server!)
+  (oz/view! example {:port 10667} )
 )
