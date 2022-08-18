e140d51626375b4818ed7f4c026554b0596c9a14
Marshall Abrams <marshall@logical.net>
2022-08-18 14:08:52 -0500

    Added end-of-walk fn, misc cleanup

--- src/clj/forage/experiment/fournier8.clj
+++ src/clj/forage/experiment/fournier8.clj
@@ -36,85 +36,20 @@
 (def fournier-mult 0.25)
 (def fournier-lvls 5)
 
-(comment
-  ;; Example Fournier foodspot configurations
-
-  ;; 5-LEVEL:
-
-  ;; Smallest horizontal/vertical gap: 195 units.
-  ;; Largest horizontal/vertical gap: 16796 units.
-  ;; Nearest horizontal/vertical distance from outer point to border: 3398.5 units.
-  ;; So the shortest horizontal/vertical toroidally across a border: 6797 units.
-  (def half-size 70000)
-  (def fournier-init-offset 200000)
-  (def fournier-mult 0.25)
-  (def fournier-lvls 5)
-  ;; Or if half-size is changed to 75000,
-  (def half-size 75000) ; then 
-  ;; nearest horizontal/vertical distance from outer point to border: 8398.5 units.
-  ;; So the shortest horizontal/vertical toroidally across a border: 16797 units,
-  ;; i.e. almost same as internal largest gap.
-
-  ;; 4-LEVEL:
-
-  ;; Smallest horizontal/vertical gap: 111 units.
-  ;; Largest horizontal/vertical gap: 2449 units.
-  ;; Nearest horizontal/vertical distance from outer point to border: 537 units.
-  ;; So the shortest horizontal/vertical toroidally across a border: 1074 units.
-  ;;;;;;;;
-  (def half-size 10000)
-  (def fournier-init-offset 28500)
-  (def fournier-mult 0.25)
-  (def fournier-lvls 4)
-
-  ;;;;;;;;
-  ;; Smallest horizontal/vertical gap: 10 units.
-  ;; Largest horizontal/vertical gap: 215 units.
-  ;; Nearest horizontal/vertical distance from outer point to border: 170 units.
-  ;; So the shortest horizontal/vertical toroidally across a border: 340 units.
-  (def half-size 1000)
-  (def fournier-init-offset 2500)
-  (def fournier-mult 0.25)
-  (def fournier-lvls 4)
-
-  ;; 3-LEVEL:
-
-  ;;;;;;;;
-  ;; Smallest horizontal/vertical gap: 219 units.
-  ;; Largest horizontal/vertical gap: 952.5 units.
-  ;; Nearest horizontal/vertical distance from outer point to border: 406 units.
-  ;; So the shortest horizontal/vertical toroidally across a border: 812 units.
-  (def half-size 5000)
-  (def fournier-init-offset 14000)
-  (def fournier-mult 0.25)
-  (def fournier-lvls 3)
-
-  ;;;;;;;;
-  ;; Smallest horizontal/vertical gap: 39 units.
-  ;; Largest horizontal/vertical gap: 234 units.
-  ;; Nearest horizontal/vertical distance from outer point to border: 180 units.
-  ;; So the shortest horizontal/vertical toroidally across a border: 360 units.
-  (def half-size 1000)
-  (def fournier-init-offset 2500)
-  (def fournier-mult 0.25) ; above 0.25, not really fractal: outer points too close
-  (def fournier-lvls 3)
-)
-
 (def perc-radius 1)
 
-;; FOR LEVY WALKS
 (def params (sorted-map ; sort so labels match values
-             :food-distance       nil
-             :perc-radius         perc-radius ; distance that an animal can "see" in searching for food
-             :powerlaw-min        perc-radius ; s/b >= per-radius (Viswanathan et al typically make them equal)
              :env-size            (* 2 half-size)
              :env-discretization  (* fournier-init-offset (reduce * (repeat fournier-lvls fournier-mult)))
-             :init-loc-fn  (constantly [half-size half-size])
+             :powerlaw-min        perc-radius ; s/b >= per-radius (Viswanathan et al typically make them equal)
              :maxpathlen          (* 5 half-size)  ; for straight walks, don't go too far
+             :perc-radius         perc-radius ; distance that an animal can "see" in searching for food
              :trunclen            (* 5 half-size) ; max length of any line segment
+             :init-loc-fn         (partial fr/end-of-walk [half-size half-size]) ; start from end of previous foodwalk, after starting in center.
+             ;; Always start in center:
+             ;:init-loc-fn         (constantly [half-size half-size])
+             :init-pad            (* 5 perc-radius) ; if truthy, initial loc offset by this in rand dir
              :look-eps            0.1    ; increment within segments for food check
-             :num-dirs            nil    ; split range this many times + 1 (includes range max); nil for random
-             :max-frac            0.25   ; proportion of pi to use as maximum direction (0 is min) ; ignored if num-dirs is falsey
             ))
 
 (def env 
@@ -124,13 +59,6 @@
                                fournier-mult
                                fournier-lvls)))
 
-(def eight-env 
-  (mf/make-env (params :env-discretization) (params :env-size)
-               (uf/eight-fournierize [[half-size half-size]]
-                                     fournier-init-offset
-                                     fournier-mult
-                                     fournier-lvls)))
-
 ;(f/remove-center (params :env-size) (params :env-size)  ... )
 
 ;; NOTE TOROIDAL LOOK-FN:
@@ -138,25 +66,18 @@
 
 
 (comment
-
+  ;; Display foodspot plots
   (require '[forage.viz.hanami :as h] :reload)
   (require '[oz.core :as oz])
-
-  (+ 3 (* 3 40))
-
-  ;; plot the foodspots alone:
   (oz/start-server!)
   (oz/view! (h/vega-env-plot env 2000 150))
   (oz/export! (h/vega-env-plot env 2000 125) "yo.svg")
-  (oz/view! (h/vega-env-plot env 600 235))
-  (oz/export! (h/vega-env-plot env 600 500) "yo.svg")
-  (oz/view! (h/vega-env-plot env 600 70))
-  (oz/view! (h/vega-env-plot eight-env 600 70))
-  (oz/export! (h/vega-env-plot eight-env 600 70) "yo.svg")
-  (oz/view! (h/vega-env-plot env 600 25))
-  (oz/view! (h/vega-env-plot env 600 10))
-  (oz/view! (h/vega-env-plot env 600 5))
+)
 
-) ; nil
-; nil
-; nil
+(comment
+  (def seed (r/make-seed))
+  (def data (time (fr/levy-experiments fr/default-file-prefix env params
+                                       [1.8 2.0 2.5 3.0] 
+                                       ; [1.001 1.5 1.8 2.0 2.5 3.0] 
+                                       100 seed look-fn)))
+  )
