a1f57f3796ff6b45e37a523e2067e7f95853f44c
mabrams <mabrams@uab.edu>
2022-09-23 00:12:35 -0500

    Modified to use pareto-cumulative function

--- src/clj/tips/techviz.clj
+++ src/clj/tips/techviz.clj
@@ -2,10 +2,15 @@
   (:require [tech.viz.vega :as tv]
             [utils.random :as r]))
 
-(def dist (r/make-powerlaw 1 2))
+(def seed (r/make-seed))
+(def rng (r/make-well19937 seed))
+(def dist (r/make-powerlaw rng 1 2))
 
 (def levy-nums (repeatedly #(r/next-double dist)))
-(def inverse-nums (map #(.cumulativeProbability dist %) levy-nums))
+(def inverse-nums (map (partial r/pareto-cumulative 1 2) levy-nums))
+
+;; obsolete I think--worked with earlier version Apache libs I was using:
+;(def inverse-nums (map #(.cumulativeProbability dist %) levy-nums))
 
 (defn hist
   "Slightly more convenient than tv/histogram, but with limitations.
@@ -15,6 +20,9 @@
   ([xs bins] (tv/histogram xs "x" {:bin-count bins})))
 
 (comment
-(oz/view! (hist (take 100000 inverse-nums)))
+  (require '[oz.core :as oz])
+  (oz/start-server!)
+  (oz/view! (hist (take 100 levy-nums)))
+  (oz/view! (hist (take 100000 inverse-nums)))
 )
 
