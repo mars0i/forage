18464af20458cfd3053420a54ef5d26ceba0d9c9
Marshall Abrams <marshall@logical.net>
2022-09-29 08:38:48 -0500

    Modified fn calls for new version of random.clj, systematized experiments

--- src/clj/forage/experiment/levy2uniform.clj
+++ src/clj/forage/experiment/levy2uniform.clj
@@ -3,14 +3,22 @@
 ;; distribution.  (Originally, this was a copy of tips/techviz.clj)
 (ns experiment.levy2uniform
   (:require [tech.viz.vega :as tv]
+            [utils.math :as m]
             [utils.random :as r]))
+
+(def mu 2)
+(def minval 1)
+(def maxval 100) ; for truncated Lévy walk
 
 (def seed (r/make-seed))
 (def rng (r/make-well19937 seed))
-(def dist (r/make-powerlaw rng 1 2))
+(def dist (r/make-powerlaw rng minval mu))
 
-(def levy-nums (repeatedly #(r/next-double dist)))
-(def inverse-nums (map (partial r/pareto-cumulative 1 2) levy-nums))
+(def pure-levy-nums (repeatedly #(r/next-double dist)))
+(def pure-inverse-nums (map (partial r/powerlaw-cumulative mu minval) pure-levy-nums))
+
+(def trunc-levy-nums (repeatedly #(r/next-double dist minval maxval)))
+(def trunc-inverse-nums (map (partial r/powerlaw-cumulative mu minval maxval) trunc-levy-nums))
 
 (defn hist
   "Slightly more convenient than tv/histogram, but with limitations.
@@ -22,7 +30,13 @@
 (comment
   (require '[oz.core :as oz])
   (oz/start-server!)
-  (oz/view! (hist (take 1000 levy-nums)))
-  (oz/view! (hist (take 10000 inverse-nums)))
+  (oz/view! (hist (take 10000 trunc-levy-nums)))
+  (oz/view! (hist (take 10000 trunc-inverse-nums)))
+  (m/mean (take 10000 trunc-inverse-nums))
+
+  (oz/view! (hist (take 1000 pure-levy-nums)))
+  (oz/view! (hist (take 100000 pure-inverse-nums)))
+  (m/mean (take 10000 pure-inverse-nums))
+
 )
 
