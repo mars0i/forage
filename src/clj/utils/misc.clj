9308422aaa979dbe810bf2c52ac1e20184466c18
Marshall Abrams <marshall@logical.net>
2022-12-31 15:31:23 -0600

    Added inclusive range function

--- src/clj/utils/misc.clj
+++ src/clj/utils/misc.clj
@@ -24,3 +24,15 @@
   "Returns true iff running vim-iced connected to a \"jack in\" nrepl."
   []
   (= "dumb" (System/getenv "TERM")))
+
+
+;; By John Collins at https://stackoverflow.com/a/68476365/1455243
+(defn irange
+  "Inclusive range function: end element is included."
+  ([start end step]
+   (take-while (if (pos? step) #(<= % end) #(>= % end)) (iterate #(+ % step) start)))
+  ([start end]
+   (irange start end 1))
+  ([end]
+   (irange 0 end))
+  ([] (range)))
