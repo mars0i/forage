4b21d225ce18785caf712164ae76241d97b29f1b
mabrams <mabrams@uab.edu>
2023-02-03 23:43:00 -0600

    more experiments and notes

--- src/clj/forage/experiment/grid12founddist.clj
+++ src/clj/forage/experiment/grid12founddist.clj
@@ -11,6 +11,7 @@
             [forage.mason.foodspot :as mf]
             [utils.random :as r]
             [utils.math :as m]
+            [forage.viz.hanami :as h]
             ;; note difference: e.g. numeric-tower/sqrt returns integer if args are
             [clojure.math.numeric-tower :as nm]
             [clojure.math :as cm] ; new in Clojure 1.11 
@@ -102,7 +103,10 @@
   (def data-rng-symm
     (time (fr/levy-experiments fr/default-file-prefix nocenter-env params
                                [1.001 1.5 2.0 2.5 3.0] 2500 seed noctr-nontoroidal-look-fn)))
-  ;; NOTE results with seed -7000696268516127574: Ballistic is best!
+  ;; NOTE RESULTS, WITH SEED -7000696268516127574: BALLISTIC IS BEST!
+  ;;    (Suggesting that the original 1D model is right, and that the reason that
+  ;;    mu=2 is effective in some envs with destructive searches is because clumpiness
+  ;;    favors local searches after a target discovery.)
   ; eval (effective-root-form): (def data-rng-symm (time (fr/...
   ; (out) Performing 12500 runs in groups of 2500 ...
   ; (out) group 1 [exponent 1.001, init-dir nil] ... "Elapsed time: 616238.658119 msecs"
@@ -123,14 +127,32 @@
   (def data-rng-asymm
     (time (fr/levy-experiments fr/default-file-prefix centered-env params
                                [1.001 1.5 2.0 2.5 3.0] 2500 seed ctrd-nontoroidal-look-fn)))
-  ;; NOTE results with seed 2928067760880362018
+  ;; NOTE results, with seed 2928067760880362018:
+  ;;    (Oh, but Wow--ballistic is best here, too.  Even though it's nondestructive.
+  ;;    Maybe suggesting that the target distribution is too sparse--everywhere, since
+  ;;    there are no clusters--for any kind of local exploration to make a big difference.)
+  ; eval (effective-root-form): (def data-rng-asymm (time (fr...
+  ; (out) Performing 12500 runs in groups of 2500 ...
+  ; (out) group 1 [exponent 1.001, init-dir nil] ... "Elapsed time: 585857.280044 msecs"
+  ; (out) num found=1540.0, efficiency=0.0000009542381809652685
+  ; (out) group 2 [exponent 1.5, init-dir nil] ... "Elapsed time: 608673.547049 msecs"
+  ; (out) num found=1511.0, efficiency=0.000000922753619211412
+  ; (out) group 3 [exponent 2.0, init-dir nil] ... "Elapsed time: 806569.929157 msecs"
+  ; (out) num found=1411.0, efficiency=0.0000008226686173127823
+  ; (out) group 4 [exponent 2.5, init-dir nil] ... "Elapsed time: 1460506.618447 msecs"
+  ; (out) num found=989.0, efficiency=0.0000005024364702998207
+  ; (out) group 5 [exponent 3.0, init-dir nil] ... 
+  ; (out) "Elapsed time: 2044073.047358 msecs"
+  ; (out) num found=690.0, efficiency=0.0000003210791658498639
+  ; (out)  done.
+  ; (out) "Elapsed time: 5505735.320035 msecs"
 
 
 
 
   ;; Write the found coordinates to csv files for later analysis:
   (def found-coords (:found-coords data-rng-symm))
-  (def found-coords (:found-coords data-rng-ssymm))
+  (def found-coords (:found-coords data-rng-asymm))
   (map (fn [xs] (count (filter identity xs))) found-coords)
   (fr/spit-csv "foundcoords1001.csv" (nth found-coords 0))
   (fr/spit-csv "foundcoords15.csv"   (nth found-coords 1))
@@ -152,72 +174,42 @@
   (def coords
     (with-open [reader
                 (clojure.java.io/reader
+                  "../../data.foraging/forage/grid12destructive-7000696268516127574/foundcoords25.csv")]
                   ;"../../data.foraging/forage/grid11destructive-7253525328864205396/foundcoords1001.csv")]
-                  "../../data.foraging/forage/grid11destructive-7253525328864205396/foundcoords20.csv")]
-                  ;"../../data.foraging/forage/grid11destructive-7253525328864205396/foundcoords30.csv")]
       (doall (clojure.data.csv/read-csv reader))))
-  (count coords)
-  (def coordsmap (doall (map (fn [[x y]] {"x" (clojure.edn/read-string x)
-                                          "y" (clojure.edn/read-string y)}) coords)))
-  (count coordsmap)
-  (def coordsmap-nonil (doall (filter (fn [{x "x", y "y"}] (or x y)) coordsmap)))
-  (count coordsmap-nonil)
 
-  (defn count-at
-    [data x y]
-    (count 
-      (filter (fn [{x' "x", y' "y"}]
-                (and (= x x') (= y y')))
-              data)))
+  ;(def coordsmap (doall (map (fn [[x y]] {"x" (clojure.edn/read-string x) "y" (clojure.edn/read-string y)}) coords)))
+  ;(def coordsmap-nonil (doall (filter (fn [{x "x", y "y"}] (or x y)) coordsmap)))
 
-  (count-at coordsmap-nonil 1001000 1001000 )
+  (def coordsmap-nonil (doall (->> coords
+                                   (map (fn [[x y]] {"x" (clojure.edn/read-string x)
+                                                     "y" (clojure.edn/read-string y)}))
+                                   (filter (fn [{x "x", y "y"}] (or x y))))))
 
-  (def sorted-coordsmap
-    (sort (fn [{x1 "x", y1 "y"} {x2 "x", y2 "y"}] (compare [x1 y1] [x2 y2]))
-	  coordsmap-nonil))
+  (def cmap1001n (filter (fn [{x "x", y "y"}] (or x y)) coords1001))
+  (def cmap15n (filter (fn [{x "x", y "y"}] (or x y)) coords15))
+  (def cmap20n (filter (fn [{x "x", y "y"}] (or x y)) coords20))
+  (def cmap25n (filter (fn [{x "x", y "y"}] (or x y)) coords25))
+  (def cmap30n (filter (fn [{x "x", y "y"}] (or x y)) coords30))
 
-  (def cmap1001 coordsmap-nonil)
-  (def cmap30 coordsmap-nonil)
-  (def cmap20 coordsmap-nonil)
-
-  (take 5 cmap1001)
-
-  ;; Experiment with normalizing distances.
-  ;; FIXME This is definitely not doing what I intended
-  (defn normalize-linear
-    [init-x init-y coordsmap]
-    (map (fn [{x "x", y "y", :as coords}] ; note vega-lite keys are strings
-           (let [x-dist (- x init-x)
-                 y-dist (- y init-y)
-                 dist-from-init (cm/sqrt (+ (* x-dist x-dist)
-                                            (* y-dist y-dist)))
-                 normalized-x (/ x dist-from-init)  ; is this what I want?
-                 normalized-y (/ y dist-from-init)]
-             {"x" normalized-x, "y" normalized-y}))
-         coordsmap))
-
-  (take 5 cmap1001)
-  (def cmap1001linear (normalize-linear half-size half-size cmap1001))
-  (take 5 cmap1001linear)
 
   (require '[oz.core :as oz])
   (oz/start-server!)
 
+  (oz/view! (h/make-heatmap 800 0 (* 2 half-size) 10000 cmap1001n))
+  (oz/view! (h/make-heatmap 800 0 (* 2 half-size) 10000 cmap15n))
+  (oz/view! (h/make-heatmap 800 0 (* 2 half-size) 10000 cmap20n))
+  (oz/view! (h/make-heatmap 800 990000 1010000 100 cmap20n))
+  (oz/view! (h/make-heatmap 800 0 (* 2 half-size) 10000 cmap25n))
+  (oz/view! (h/make-heatmap 800 950000 1050000 500 cmap25n))
+  (oz/view! (h/make-heatmap 800 0 (* 2 half-size) 10000 cmap30n))
+  (oz/view! (h/make-heatmap 800 990000 1010000 200 cmap30))
+  (oz/view! (h/make-heatmap 800 995000 1005000 50 cmap30))
 
+  (oz/view! (h/make-heatmap 800 0 (int (cm/sqrt (* 2 half-size))) 30 cmap1001linear))
 
-  ;; NOTE
-  ;; THESE ARE NOT THE SAME.  The tooltip apparently causes filtering.  
-  ;; I think it causes only counting at a single coordinate.
-  (oz/view! (make-heatmap 800 0 (* 2 half-size) 10000 cmap1001))
-  (oz/view! (make-heatmap 800 0 (* 2 half-size) 10000 cmap20))
-  (oz/view! (make-heatmap 800 800000  1200000   10000 cmap20))
-  (oz/view! (make-heatmap 800 0 (* 2 half-size) 10000 cmap30))
+  (oz/view! (h/make-heatmap 800 600000 1400000 5000 cmap20))
 
-  (oz/view! (make-heatmap 800 0 (int (cm/sqrt (* 2 half-size))) 30 cmap1001linear))
-
-  (oz/view! (make-heatmap 800 600000 1400000 5000 cmap20))
-
-  (oz/view! (make-heatmap 800 990000 1010000 500 cmap30))
 
 
 
@@ -235,6 +227,33 @@
 
 
 
+  (defn count-at
+    [data x y]
+    (count 
+      (filter (fn [{x' "x", y' "y"}]
+                (and (= x x') (= y y')))
+              data)))
 
+  (count-at coordsmap-nonil 1001000 1001000 )
+
+
+  (def sorted-coordsmap
+    (sort (fn [{x1 "x", y1 "y"} {x2 "x", y2 "y"}] (compare [x1 y1] [x2 y2]))
+	  coordsmap-nonil))
+
+
+  ;; Experiment with normalizing distances.
+  ;; FIXME This is definitely not doing what I intended
+  (defn normalize-linear
+    [init-x init-y coordsmap]
+    (map (fn [{x "x", y "y", :as coords}] ; note vega-lite keys are strings
+           (let [x-dist (- x init-x)
+                 y-dist (- y init-y)
+                 dist-from-init (cm/sqrt (+ (* x-dist x-dist)
+                                            (* y-dist y-dist)))
+                 normalized-x (/ x dist-from-init)  ; is this what I want?
+                 normalized-y (/ y dist-from-init)]
+             {"x" normalized-x, "y" normalized-y}))
+         coordsmap))
 
 )
