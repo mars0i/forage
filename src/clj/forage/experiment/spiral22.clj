fa18ae6f84a06a5686433e5bc64522e810a1be46
Marshall Abrams <marshall@logical.net>
2023-03-27 09:17:28 -0500

    Added experiments, misc cleanup and organization

--- src/clj/forage/experiment/spiral22.clj
+++ src/clj/forage/experiment/spiral22.clj
@@ -28,12 +28,12 @@
              :powerlaw-min        1
              :env-size            (* 2 half-size)
              :env-discretization  5 ; for Continuous2D; see foodspot.clj
-             :init-loc-fn         (constantly [half-size half-size]) ; TODO MODIFY THIS?
+             :init-loc-fn         (constantly [half-size half-size])
              :init-pad            nil ; if truthy, initial loc offset by this in rand dir
              :maxpathlen          maxpathlen
              :trunclen            trunclen
              :look-eps            0.2    ; TODO WILL THIS WORK WITH SHORTER SPIRAL SEGMENTS?
-             :basename            (str default-dirname "spiral21_")
+             :basename            (str default-dirname "spiral22_")
              ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -75,41 +75,93 @@
 (def seed (r/make-seed))
 ;(def seed -7370724773351240133)
 (def rng (r/make-well19937 seed))
-(def mu1xdist (r/make-powerlaw rng 1 1.1))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Functions that construct component walks
+
+;; Component distributions
+(def mu1dist (r/make-powerlaw rng 1 1.1))
+(def mu15dist (r/make-powerlaw rng 1 1.5))
 (def mu3dist (r/make-powerlaw rng 1 3))
 
-(defn more-mu1x-vecs [] 
-  (w/vecs-upto-len explore-segment-len (w/make-levy-vecs rng mu1xdist 1 (params :trunclen))))
+;; Component walks
+(defn more-mu1-vecs [] 
+  (w/vecs-upto-len explore-segment-len (w/make-levy-vecs rng mu1dist 1 (params :trunclen))))
+(defn more-mu15-vecs [] 
+  (w/vecs-upto-len explore-segment-len (w/make-levy-vecs rng mu15dist 1 (params :trunclen))))
 (defn more-mu3-vecs [] 
   (w/vecs-upto-len examine-segment-len (w/make-levy-vecs rng mu3dist  1 (params :trunclen))))
 (defn more-spiral-vecs []
   (w/vecs-upto-len examine-segment-len (sp/unit-archimedean-spiral-vecs 2 0.1)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Functions that construct composite walks
+
+;; composite mu=1.1 and mu=3 walk
 (defn composite-mu1-mu3-vecs
   [maxpathlen]
   (w/vecs-upto-len maxpathlen ; vecs-upto-len is eager, so whatever it takes will be realized
                    (apply concat
-                          (interleave (repeatedly more-mu1x-vecs)
+                          (interleave (repeatedly more-mu1-vecs)
                                       (repeatedly more-mu3-vecs)))))
+
+;; composite mu=1.5 and mu=3 walk
+(defn composite-mu15-mu3-vecs
+  [maxpathlen]
+  (w/vecs-upto-len maxpathlen ; vecs-upto-len is eager, so whatever it takes will be realized
+                   (apply concat
+                          (interleave (repeatedly more-mu15-vecs)
+                                      (repeatedly more-mu3-vecs)))))
+
+;; composite mu=1.1 and spiral walk
 (defn composite-mu1-spiral-vecs
   [maxpathlen]
   (w/vecs-upto-len maxpathlen ; vecs-upto-len is eager, so whatever it takes will be realized
                    (apply concat
-                          (interleave (repeatedly more-mu1x-vecs)
+                          (interleave (repeatedly more-mu1-vecs)
                                       (repeatedly more-spiral-vecs)))))
 
-(def brown-walk-fns
+;; composite mu=1.5 and spiral walk
+(defn composite-mu15-spiral-vecs
+  [maxpathlen]
+  (w/vecs-upto-len maxpathlen ; vecs-upto-len is eager, so whatever it takes will be realized
+                   (apply concat
+                          (interleave (repeatedly more-mu15-vecs)
+                                      (repeatedly more-spiral-vecs)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Maps whose values are functions that run composite and non-composite 
+;; walks in each of the different environments defined above.
+
+;; composite mu=1.1 and mu=3
+(def mu1-mu3-walk-fns
   {"composite-brownian-env0" (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 0)) (params :look-eps) (w/walk-stops init-loc (composite-mu1-mu3-vecs (params :maxpathlen)))))
    "composite-brownian-env1" (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 1)) (params :look-eps) (w/walk-stops init-loc (composite-mu1-mu3-vecs (params :maxpathlen)))))
    "composite-brownian-env2" (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 2)) (params :look-eps) (w/walk-stops init-loc (composite-mu1-mu3-vecs (params :maxpathlen)))))
    "composite-brownian-env3" (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 3)) (params :look-eps) (w/walk-stops init-loc (composite-mu1-mu3-vecs (params :maxpathlen)))))})
 
-(def spiral-walk-fns
+;; composite mu=1.5 and mu=3
+(def mu15-mu3-walk-fns
+  {"composite-brownian-env0" (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 0)) (params :look-eps) (w/walk-stops init-loc (composite-mu15-mu3-vecs (params :maxpathlen)))))
+   "composite-brownian-env1" (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 1)) (params :look-eps) (w/walk-stops init-loc (composite-mu15-mu3-vecs (params :maxpathlen)))))
+   "composite-brownian-env2" (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 2)) (params :look-eps) (w/walk-stops init-loc (composite-mu15-mu3-vecs (params :maxpathlen)))))
+   "composite-brownian-env3" (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 3)) (params :look-eps) (w/walk-stops init-loc (composite-mu15-mu3-vecs (params :maxpathlen)))))})
+
+;; composite mu=1.1 and spiral
+(def mu1-spiral-walk-fns
   {"composite-spiral-env0"   (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 0)) (params :look-eps) (w/walk-stops init-loc (composite-mu1-spiral-vecs (params :maxpathlen)))))
    "composite-spiral-env1"   (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 1)) (params :look-eps) (w/walk-stops init-loc (composite-mu1-spiral-vecs (params :maxpathlen)))))
    "composite-spiral-env2"   (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 2)) (params :look-eps) (w/walk-stops init-loc (composite-mu1-spiral-vecs (params :maxpathlen)))))
    "composite-spiral-env3"   (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 3)) (params :look-eps) (w/walk-stops init-loc (composite-mu1-spiral-vecs (params :maxpathlen)))))})
 
+;; composite mu=1.5 and spiral
+(def mu15-spiral-walk-fns
+  {"composite-spiral-env0"   (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 0)) (params :look-eps) (w/walk-stops init-loc (composite-mu15-spiral-vecs (params :maxpathlen)))))
+   "composite-spiral-env1"   (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 1)) (params :look-eps) (w/walk-stops init-loc (composite-mu15-spiral-vecs (params :maxpathlen)))))
+   "composite-spiral-env2"   (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 2)) (params :look-eps) (w/walk-stops init-loc (composite-mu15-spiral-vecs (params :maxpathlen)))))
+   "composite-spiral-env3"   (fn [init-loc] (w/foodwalk (make-unbounded-look-fn (envs 3)) (params :look-eps) (w/walk-stops init-loc (composite-mu15-spiral-vecs (params :maxpathlen)))))})
+
+;; pure mu=2 walks
 (def mu2-walk-fns
   {"mu2-env0" (partial fr/levy-run rng (make-unbounded-look-fn (envs 0)) nil params 2.0)
    "mu2-env1" (partial fr/levy-run rng (make-unbounded-look-fn (envs 1)) nil params 2.0)
@@ -132,23 +184,37 @@
   (require '[oz.core :as oz])
   (oz/start-server!)
 
+  (def env (envs 3))
+
   (def walk1s (time (w/walk-stops [half-size half-size] (composite-mu1-spiral-vecs (params :maxpathlen)))))
-  (time (oz/view! (h/vega-envwalk-plot (envs 3) 600 0.7 1000 walk1s)))
+  (def vwalk1s (time (h/vega-envwalk-plot env 600 0.75 1000 walk1s)))
+  (time (oz/view! vwalk1s))
+
+  (def walk15 (time (w/walk-stops [half-size half-size] (composite-mu15-spiral-vecs (params :maxpathlen)))))
+  (def vwalk15 (time (h/vega-envwalk-plot env 600 0.75 1000 walk15)))
+  (time (oz/view! vwalk15))
 
   (def walk13 (time (w/walk-stops [half-size half-size] (composite-mu1-mu3-vecs (params :maxpathlen)))))
-  (def vwalk13 (time (h/vega-envwalk-plot (envs 3) 600 1.0 1000 walk13)))  ; 8 minutes
-  (time (oz/view! vwalk13)) ; returns quickly but DOESN'T DISPLAY
+  (def vwalk13 (time (h/vega-envwalk-plot env 600 0.75 1000 walk13)))  ; 8 minutes
+  (time (oz/view! vwalk13)) ; returns quickly but DOESN'T DISPLAY?
+
+  (def walk153 (time (w/walk-stops [half-size half-size] (composite-mu15-mu3-vecs (params :maxpathlen)))))
+  (def vwalk153 (time (h/vega-envwalk-plot env 600 0.75 1000 walk153)))  ; 8 minutes
+  (time (oz/view! vwalk153))
+
   ;; Try this instead:
   ;(require '[aerial.hanami.common :as hc])
-  (oz/export! vwalk13
-              "compositeBrownianWalkExampleSpiral21commit_02cbde0_seed-7370724773351240133.png")
+  ;(oz/export! vwalk13
+  ;            "compositeBrownianWalkExampleSpiral21commit_02cbde0_seed-7370724773351240133.png")
 
   ;(time (h/write-foodwalk-plots "compositemu1m3" :svg seed (envs 0) 600 1 1 1.0 0 "1.25,3" params [walk13]))
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; RUN THE EXPERIMENTS
-  (def spiral-data-and-rng (time (fr/walk-experiments (update params :basename #(str % "spiral")) spiral-walk-fns 100 seed)))
   (def mu2-data-and-rng    (time (fr/walk-experiments (update params :basename #(str % "mu2")) mu2-walk-fns 100 seed)))
-  (def brown-data-and-rng  (time (fr/walk-experiments (update params :basename #(str % "brown")) brown-walk-fns 100 seed)))
+  (def mu1-spiral-data-and-rng (time (fr/walk-experiments (update params :basename #(str % "spiral")) mu1-spiral-walk-fns 100 seed)))
+  (def mu1-mu3-data-and-rng  (time (fr/walk-experiments (update params :basename #(str % "brown")) mu1-mu3-walk-fns 100 seed)))
+  (def mu15-spiral-data-and-rng (time (fr/walk-experiments (update params :basename #(str % "spiral")) mu15-spiral-walk-fns 100 seed)))
+  (def mu15-mu3-data-and-rng  (time (fr/walk-experiments (update params :basename #(str % "brown")) mu15-mu3-walk-fns 100 seed)))
 
 )
