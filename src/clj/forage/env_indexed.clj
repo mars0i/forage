4293b084ea8679a1caf314759cdc2783ef7efd11
Marshall Abrams <marshall@logical.net>
2023-04-01 16:16:42 -0500

    Added function to set values in radius of foodspot

--- src/clj/forage/env_indexed.clj
+++ src/clj/forage/env_indexed.clj
@@ -1,6 +1,7 @@
 ;; Future home of replacement for env-mason.
 (ns forage.env-indexed
-  (:require [clojure.core.matrix :as mx]))
+  (:require [clojure.core.matrix :as mx]
+            [utils.misc :as um]))
 
 ;; Strategy:
 ;; Foodspots exist in a 2D grid made with core.matrix using one of 
@@ -11,29 +12,60 @@
 ;; foodspot it's a radius around.  This is the size of the perceptual
 ;; radius, but it's around the foodspots.
 
-;; When a walk reaches a point, mod its coords it to get the indexes into 
-;; the matrix and check wehther it's in the radius of the foodspot.
+;; These should perhaps be marked with the coordinates of the foodspoot
+;; that that can be recorded.
+
+;; Alternatively, if all paths are continuous in 2D, one might only fill in
+;; the slots on the perimeter.
+
+;; When a walk reaches a point, mod or better yet round its coords it to
+;; get the indexes into the matrix and check whether it's in the radius
+;; of the foodspot.
+
+;; The representation should have a scale, so that e.g. we represent a
+;; distance of 1 as 100 cells.  Otherwise, if a distance of 1 was the cell
+;; dimension, then perceptual radius 1 would be represented by a Moore or
+;; von Neumann neighborhood, which is not a good approximation of circle.
+
+;; PROBLEM: What is perc-radii for two foodspots overlap?
 
 
-;(mx/set-current-implementation :persistent-vector)
-(mx/set-current-implementation :ndarray)
+; (mx/set-current-implementation :persistent-vector)  ; doesn't allow mset!
+(mx/set-current-implementation :ndarray) 
 ;(mx/set-current-implementation :vectorz)
 
+;(def scale 1000)
+
 (defn make-env
-  "Creates a new environment in the form of a matrix.  If locs is
-  provided, it should be a collection of triples consisting of two
-  integers representing coordinates of a location in the environment,
-  and a value to be placed at that location."
-  ([size] (mx/new-matrix size size))
-  ([size locs]
-   (let [e (make-env size)]
-     (doseq [[x y v] locs]
-       (mx/mset! e x y v))
-     e)))
+  "Creates a new environment in the form of a matrix with cells initialized
+  with nils."
+  [size] 
+  (let [env (mx/new-matrix size size)]
+    (doseq [x (range size)
+            y (range size)]
+      (mx/mset! env x y nil))
+    e))
+
+
+(defn add-foodspot!
+  "Add foodspot, with its perceptual radius perc-radius, at scale scale, at
+  location (x y). All cells within perc-radius of (x y) will have the value
+  [x y]."
+  [env scale x y perc-radius]
+  (let [radius-squared (* perc-radius perc-radius)]
+    (doseq [off-x (um/irange (- scale) scale)
+            off-y (um/irange (- scale) scale)
+            :when (<= (+ (* off-x off-x) (* off-y off-y))
+                      radius-squared)]
+      (mx/mset! env (+ x off-x) (+ y off-y) [x y])))) ; Every within radius contains spot coords, including itself
 
 
 (comment
-  (mx/pm
-    (make-env 5 [[1 1 1] [1 2 "Yow!"] [4 3 27]])
-  )
+  (def e (make-env 10))
+  (class e)
+  (mx/pm e)
+  (mx/mset! e 1 2 15)
+  (add-foodspot! e 1 4 4 3)
+
+
 )
