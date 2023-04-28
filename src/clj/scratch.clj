d5a180512a4736ce521ef7ae95bb9ec65111dff0
mabrams <mabrams@uab.edu>
2023-04-28 10:27:06 -0500

    Illustration of core.matrix "bug".

--- src/clj/scratch.clj
+++ src/clj/scratch.clj
@@ -1,11 +1,12 @@
 ;; File for misc experiments.  Think of it as a repl.
 (ns scratch
   (:require
-   [utils.random :as ur]
-   [utils.math :as um]
-   [utils.toroidal :as t]
-   [forage.food :as f]
-   [forage.walks :as w]
+    [clojure.core.matrix :as mx]
+   ;[utils.random :as ur]
+   ;[utils.math :as um]
+   ;[utils.toroidal :as t]
+   ;[forage.food :as f]
+   ;[forage.walks :as w]
    ;[clojure.math.numeric-tower :as nt]
    ;[oz.core :as oz]
    ;[forage.viz.hanami :as h]   ; don't load with cljplot
@@ -19,77 +20,26 @@
 ;; passed to the fn, but that would only matter in an inner loop.
 
 
-(def maxpathlen 100)
-
-(defn realized-fitness-benefit
-  [benefit success]
-  (* benefit success))
-
-(defn realized-fitness-cost
-  [cost pathlen]
-  (* cost pathlen))
-
-(defn realized-fitness
-  [benefit cost [success pathlen]]
-  (- (* benefit success) (* cost pathlen)))
-
 
 (comment
+  (def m (mx/matrix :ndarray [[nil nil nil] [nil nil nil] [nil nil nil]]))
+  (mx/mset! m 0 1 (list :a))
+  (mx/pm m) ; succeeds
+  (mx/mset! m 0 0 (list :a))
+  (mx/shape m) ; => [3 3]
+  (mx/pm m)
+; eval (effective-root-form): (mx/pm m)
+; (err) Execution error (ExceptionInfo) at clojure.core.matrix.impl.persistent-vector/eval22141$fn (persistent_vector.cljc:571).
+; (err) Can't convert to persistent vector array: inconsistent shape.
+  (mx/mset! m 0 0 nil) ; This fixes the problem
 
-  (def results1 [[1 (* maxpathlen 0.5)]
-                 [1 (* maxpathlen 0.5)]
-                 [1 (* maxpathlen 0.5)]
-                 [1 (* maxpathlen 0.5)]
-                 [1 (* maxpathlen 0.5)]
-                 [1 (* maxpathlen 0.5)]
-                 [1 (* maxpathlen 0.5)]
-                 [1 (* maxpathlen 0.5)]
-                 [1 (* maxpathlen 0.5)]
-                 [1 (* maxpathlen 0.5)]])
+;; These are also not OK:
+  (mx/mset! m 1 0 (range 1))
+  (mx/mset! m 2 0 [:a])
 
-  (def results2 [[1 (* maxpathlen 0.1)]
-                 [1 (* maxpathlen 0.1)]
-                 [1 (* maxpathlen 0.3)]
-                 [1 (* maxpathlen 0.4)]
-                 [1 (* maxpathlen 0.5)]
-                 [1 (* maxpathlen 0.6)]
-                 [1 (* maxpathlen 0.7)]
-                 [1 (* maxpathlen 0.8)]
-                 [1 (* maxpathlen 0.9)]
-                 [1 (* maxpathlen 1.0)]])
-
-  (def results3 [[1 (* maxpathlen 0.1)]
-                 [1 (* maxpathlen 0.1)]
-                 [1 (* maxpathlen 0.1)]
-                 [1 (* maxpathlen 0.1)]
-                 [1 (* maxpathlen 0.1)]
-                 [1 maxpathlen]
-                 [1 maxpathlen]
-                 [1 maxpathlen]
-                 [1 maxpathlen]
-                 [1 maxpathlen]])
-
-  (def results4 [[0 maxpathlen]
-                 [0 maxpathlen]
-                 [0 maxpathlen]
-                 [0 maxpathlen]
-                 [0 maxpathlen]
-                 [1 maxpathlen]
-                 [1 maxpathlen]
-                 [1 maxpathlen]
-                 [1 maxpathlen]
-                 [1 maxpathlen]])
-
-  (map ($ realized-fitness maxpathlen 1) results7)
-  (um/variance (map ($ realized-fitness maxpathlen 1) results1))
-  (um/variance (map ($ realized-fitness maxpathlen 1) results2))
-  (um/variance (map ($ realized-fitness maxpathlen 1) results3))
-  (um/variance (map ($ realized-fitness maxpathlen 1) results4))
-
-  (um/sample-variance (map ($ realized-fitness maxpathlen 1) results1))
-  (um/sample-variance (map ($ realized-fitness maxpathlen 1) results2))
-  (um/sample-variance (map ($ realized-fitness maxpathlen 1) results3))
-  (um/sample-variance (map ($ realized-fitness maxpathlen 1) results4))
+  ;; These don't cause problems:
+  (mx/mset! m 0 0 #{:a})
+  (mx/mset! m 0 0 {:a 1})
 )
 
 
