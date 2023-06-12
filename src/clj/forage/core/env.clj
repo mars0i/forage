533e8443104f65300141de884de7eae2dbd9f47e
Marshall Abrams <marshall@logical.net>
2023-06-12 11:37:51 -0500

    Renamed env-setup to env

--- src/clj/forage/core/env.clj
+++ src/clj/forage/core/env.clj
@@ -1,38 +1,42 @@
-;; ABANDONED
-;; I think this is a bad idea. Some of the functions worth adding might end
-;; being used in performance-critical sections, protocols do runtime
+;; Namespace for global configuration to use one or another environment
+;implementation.  Edit this file to switch between env-mason and env-matrix.
+(ns forage.core.env)
+
+;; Switch between different environment implementations.
+;; (This will affect functions that make use of an alias that is set to 
+;; whatever is referenced here. Functions can neverthless refer to a 
+;; specific implementation using other aliases.)
+(def env 'forage.core.env-mason)
+;(def env 'forage.core.env-matrix)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Code below is ABANDONED
+;; I think it was a bad idea. Some of the functions worth adding might end
+;; being used in performance-critical sections, and protocols do runtime
 ;; dispatch.  The perf hit might be negligible, but I just don't really
-;; need this, I think.  Better to just change namespaces by hand or something.
-
-
-;; protocol for environments
-;; I'd rather implement this all in one place rather than in the
-;; source files for env implementations.
-(ns forage.core.env
-  (:require [forage.core.env-mason :as mas]
-            [forage.core.env-matrix :as mat])
-  ;; Not needed:
-  ;(:import [sim.field.continuous Continuous2D]
-  ;         [forage.core.env_matrix MatEnv])
-  )
-
-(defprotocol Env
-  (env-size [env])
-  (env-foodspot-coords [env])
-  ;(perc-first-foodspot [env x y]) ; ??  cf. run.clj
-  ;; other stuff
-)
-
-
-;; Using low-level extend rather than extend-protocol so that
-;; I can set methods equal to functions defined in other files.
-
-;; Continuous2D is the Java class for MASON-based environments
-(extend Continuous2D 
-  Env {:env-size mas/env-size
-       :env-foodspot-coords mas/env-foodspot-coords})
-
-;; MatEnv is defined by defrecord in env-matrix.
-(extend MatEnv
-  Env {:env-size mas/env-size
-       :env-foodspot-coords mas/env-foodspot-coords})
+;; need this, I think.  There are better solutions, I think.  (I do need
+;; to be able to switch between the different environment implementations easily.
+;; I don't need to be able to run different environment configs from the
+;; same set of main lib files, unchanged.)
+;
+; (defprotocol Env
+;   (env-size [env])
+;   (env-foodspot-coords [env])
+;   ;(perc-first-foodspot [env x y]) ; ??  cf. run.clj
+;   ;; other stuff
+; )
+; 
+; 
+; ;; Using low-level extend rather than extend-protocol so that
+; ;; I can set methods equal to functions defined in other files.
+; 
+; ;; Continuous2D is the Java class for MASON-based environments
+; (extend Continuous2D 
+;   Env {:env-size mas/env-size
+;        :env-foodspot-coords mas/env-foodspot-coords})
+; 
+; ;; MatEnv is defined by defrecord in env-matrix.
+; (extend MatEnv
+;   Env {:env-size mas/env-size
+;        :env-foodspot-coords mas/env-foodspot-coords})
