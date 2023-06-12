b7e0248ab8257e8e5786627b75de85a4eaeb9aeb
Marshall Abrams <marshall@logical.net>
2023-06-12 11:39:34 -0500

    Renamed env-setup to env

--- src/clj/forage/core/env_setup.clj
+++
@@ -1,7 +0,0 @@
-;; Namespace for global configuration to use one or another environment
-;implementation.  Edit this file to switch between env-mason and
-;env-matrix.
-(ns forage.core.env-setup)
-
-(def env 'forage.core.env-mason)
-;(def env 'forage.core.env-matrix)
