3e47b8d1e8e04bf10d7142a6de7ee6e3738d436d
mabrams <mabrams@uab.edu>
2023-09-18 15:40:01 -0500

    Added experiments with adding indiv fitness column

--- src/clj/forage/experiment/spiral23data.clj
+++ src/clj/forage/experiment/spiral23data.clj
@@ -1,7 +1,7 @@
 (ns forage.experiment.spiral23data
   (:require [tech.v3.dataset :as ds]
             ;[tech.v3.datatype.functional :as dsf]
-            ;[tablecloth.api :as tc]
+            [tablecloth.api :as tc]
             [scicloj.clay.v2.api :as clay]
             [scicloj.kindly.v4.api :as kindly]
             [scicloj.kindly.v4.kind :as kind]
@@ -22,10 +22,44 @@
   (memoize slurp))
 
 (comment 
+
+  ;; doesn't work
+  (def spiral23fityo
+    (ds/row-map spiral23
+                (fn [{:keys [length found]}]
+                  (fit/cost-benefit-fitness 1 0.0001 found length))))
+
+  ;; works, but there has to be a better way
+  (def spiral23fityo
+    (ds/row-map spiral23
+                (fn [row] 
+                  {:env (row :env)
+                   :walk (row :walk)
+                   :length (row :length)
+                   :found (row :found)
+                   :fit (fit/cost-benefit-fitness 100 1 0.0001 (row :found) (row :length))})))
+
+
+  ;; this is a little better, but there still must be a better way
+  (def spiral23fityo
+    (ds/row-map spiral23
+                (fn [{:keys [env walk length found]}]
+                  {:env env
+                   :walk walk
+                   :length length
+                   :found found
+                   :fit (fit/cost-benefit-fitness 100 1 0.0001 found length)})))
+
+  (ds/descriptive-stats spiral23fityo)
+
+
   (clojure.repl/dir clay)
 
   (clay/start!)
   (clay/browse!)
+
+  (clay/handle-value! (ds/descriptive-stats spiral23))
+  (clay/handle-value! spiral23)
 
   ;; show-doc! is obsolete
   (clay/show-namespace! "src/clj/forage/experiment/spiral23data.clj") 
