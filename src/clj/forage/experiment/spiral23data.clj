349f896b4094ec8fea73f157d149e3ff4766e37e
Marshall Abrams <marshall@logical.net>
2023-09-19 08:49:09 -0500

    Added new experiments calculating fitnesses

--- src/clj/forage/experiment/spiral23data.clj
+++ src/clj/forage/experiment/spiral23data.clj
@@ -14,46 +14,70 @@
 (def spiral23filepath (str home fileloc spiral23filename))
 
 ;; Load base data for further use
-(def spiral23 (ds/->dataset spiral23filepath))
+(defonce spiral23 (ds/->dataset spiral23filepath))
 (comment (ds/descriptive-stats spiral23) )
 
-;; Trying stuff from https://scicloj.github.io/clay/
-(defonce memoized-slurp
-  (memoize slurp))
 
 (comment 
 
-  ;; tc/aggregate
-
-  ;; doesn't work
-  (def spiral23fityo
-    (ds/row-map spiral23
-                (fn [{:keys [length found]}]
-                  (fit/cost-benefit-fitness 1 0.0001 found length))))
+  ;; ADDING AN INDIV FITNESS COLUMN:
 
   ;; works, but there has to be a better way
-  (def spiral23fityo
+  (def spiral23-ifit
     (ds/row-map spiral23
                 (fn [row] 
                   {:env (row :env)
                    :walk (row :walk)
                    :length (row :length)
                    :found (row :found)
-                   :fit (fit/cost-benefit-fitness 100 1 0.0001 (row :found) (row :length))})))
+                   :ifit (fit/cost-benefit-fitness 100 1 0.0001 (row :found) (row :length))})))
 
 
   ;; this is a little better, but there still must be a better way
-  (def spiral23fityo
+  (def spiral23-ifit
     (ds/row-map spiral23
                 (fn [{:keys [env walk length found]}]
                   {:env env
                    :walk walk
                    :length length
                    :found found
-                   :fit (fit/cost-benefit-fitness 100 1 0.0001 found length)})))
+                   :ifit (fit/cost-benefit-fitness 100 1 0.0001 found length)})))
 
-  (ds/descriptive-stats spiral23fityo)
+  ;; Tablecloth attempt.  Doesn't work. Also, even if it runs, it's not
+  ;; what I want, because with add-column, "When function is used, argument
+  ;; is whole dataset and the result should be column, sequence or single value"
+  (def spiral23-ifit
+    (tc/add-column spiral23
+                   :ifit
+                   (fn [{:keys [found length]}]
+                     (fit/cost-benefit-fitness 100 1 0.0001 found length))))
 
+  (ds/descriptive-stats spiral23-ifit)
+
+
+  ;; CALCULATE PER-CONFIG TRAIT FITNESS ESTIMATES:
+
+  ;; Note this example from https://scicloj.github.io/tablecloth/index.html#Aggregate
+  ; (-> DS
+  ;     (tc/group-by [:V4])
+  ;     (tc/aggregate [#(take 3 (% :V2))
+  ;                    (fn [ds] {:sum-v1 (reduce + (ds :V1))
+  ;                              :prod-v3 (reduce * (ds :V3))})] {:default-column-name-prefix "V2-value"}))
+  ;
+  ;; (Also see the following example if I want a "grouped" dataset as output.)
+
+  ;; Here is my version of the tablecloth example:
+  (def spiral23-tfit
+    (-> spiral23-ifit
+        (tc/group-by [:env :walk])
+        (tc/aggregate 
+          {:trait-fit (fn [{:keys [ifit]}] ; could also calc on the fly from found, length
+                        (fit/sample-gillespie-dev-stoch-fitness ifit))})))
+
+  (ds/descriptive-stats spiral23-tfit)
+
+
+  ;; CLAY EXPERIMENTS
 
   (clojure.repl/dir clay)
 
