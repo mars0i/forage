e95b85385534cac7139a3d1372bf1934381299f4
Marshall Abrams <marshall@logical.net>
2023-10-19 12:33:02 -0500

    Minor formatting

--- src/clj/utils/math.clj
+++ src/clj/utils/math.clj
@@ -8,10 +8,10 @@
               [clojure.core :as cc] ; to replace fastmath macros in reduce, map, etc.
               [clojure.string :as st]))
 
-;(set! *warn-on-reflection* true)
-;(set! *unchecked-math* :warn-on-boxed)
-
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
 (fm/use-primitive-operators)
+
 
 (defn remove-decimal-pt
   "Given a number, returns a (base-10) string representation of the
