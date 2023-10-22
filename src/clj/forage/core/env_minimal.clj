1701c0579b9ec3c920040cff075a92af49551f7c
Marshall Abrams <marshall@logical.net>
2023-10-22 16:17:22 -0500

    Improved docstrings and organization

--- src/clj/forage/core/env_minimal.clj
+++ src/clj/forage/core/env_minimal.clj
@@ -1,28 +1,44 @@
-;; Functions for a minimal, fast environment containing a 
+;; Functions for minimal, fast environments containing a 
 ;; single or a few foodspots, without toroidal lookup.
 (ns forage.core.env-minimal
   (:require [utils.math :as um]
             [forage.core.food :as f]))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ENVIRONMENT THAT CONSISTS OF A BARE COORDINATE PAIR
+
 ;; NOTE Since no size, doesn't support toroidal lookup.
 (defn make-single-foodspot-env
+  "Make an environment that consists of a single foodspog, and only a
+  single point.  The environment returned is is simply a pair [x y] of
+  coordinates for that foodspot."
   [x y]
   [x y])
 
-(defn make-multiple-foodspot-env
-  "coords should be a sequence of x,y pairs."
-  [coords]
-  (vec coords))
-
 (defn perc-single-foodspot
-  "Returns a vector containing the sole foodspot if it's within perc-radius
-  of forager-coords (an x, y Clojure pair), or nil if not.  Example usage:
-    (def look-fn (partial env (params :perc-radius)))"
+  "Returns a collection containing the sole foodspot if it's within
+  perc-radius of forager-coords (an x, y Clojure pair), or nil if not.
+  This is designed to work *only* with environments that are nothing more
+  than a pair representing a single foodspot, as created by
+  make-single-food-spot-env. If the env has only a single foodspot,
+  but it's in a collection, use perc-mulitple-foodspots instead.
+  Example usage:
+     (def look-fn (partial env (params :perc-radius)))"
   [env perc-radius forager-coords]
   (if (<= (um/distance-2D forager-coords env) perc-radius)
     [env]
     nil))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ENVIRONMENT THAT CONSISTS OF A COLLECTION of COORDINATE PAIRS.
+
+(defn make-multiple-foodspot-env
+  "Make an environment that consists of a vector of pairs representing
+  foodspots. coords should be a sequence of x,y pairs."
+  [coords]
+  (vec coords))
 
 (defn perc-multiple-foodspots
   "Returns a vector containing the the first foodspot within perc-radius
