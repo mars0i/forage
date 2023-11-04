fe7f7cda288a990c0d3532f1bb3a9528f188c16b
mabrams <mabrams@uab.edu>
2023-11-04 01:14:45 -0500

    Fixed indexing bug, attempted ham-fisted destructuring let
    I think I need to create a custom let using extend-let to do
    that here.

--- src/clj/forage/core/env_minimal.clj
+++ src/clj/forage/core/env_minimal.clj
@@ -2,6 +2,7 @@
 ;; few foodspots, without toroidal lookup.
 (ns forage.core.env-minimal
   (:require [ham-fisted.api :as hf]
+            [ham-fisted.hlet :as hfl]
             [fastmath.core :as fm]
             [utils.math :as um]
             [forage.core.food :as f]))
@@ -73,25 +74,28 @@
   env)
 
 
+;; env-mason look-fns take a pair of coordinates representing the current
+;; location, so that Continuous2D can look for any targets in the nearest bucket.
+;; In env-minimal returns the same collection of targets no matter where you are,
+;; so there's no need to pass in coordinates.  So make-look-fn returns a
+;; function of no arguments that always returns the same targets.
+;;
 ;; I could randomize the order of foodspots with a different look-fn.
 (defn make-look-fn
   [env ^double perc-radius]
   (constantly
     (hf/double-array
-      (into [perc-radius (inc (count env))] ; second element is index of last coordinate
+      (into [perc-radius (+ 1.0 (* 2.0 (count env)))] ; second element is index of last coordinate
             (apply concat env)))))
 
-
-
-;; Note that look-fn plays a different role here than in walks/find-in-seg,
-;; as it must.
+;; Note that look-fn plays a different role here than in walks/find-in-seg, as it must.
 (defn find-in-seg
   "Only returns the first foodspot found.  The search in order that
   foodspots are returned by look-fn."
   [look-fn _ x0 y0 x1 y1]
   (let [^doubles info (look-fn)
         perc-radius (hf/dnth info 0)
-        last-index (inc (long (hf/dnth info 1))) ; env size + 2
+        last-index (long (hf/dnth info 1)) ; env size + 2
         near-pt-fn (partial um/near-pt-on-seg x0 y0 x1 y1)] ; Is this a good idea?
     (loop [i 2]
       (let [j (inc i)
@@ -103,6 +107,39 @@
             distance (um/distance-2D* near-x near-y p q)]
         (cond (<= distance perc-radius) [[[p q]] [near-x near-y]] ; seq of single foodspot found, where found from
               (= j last-index) nil
-              :else (recur (inc i)))))))
+              :else (recur (+ 2 i)))))))
 
+
+(comment
+  ;; The new ham-fisted let is not yet flexible enough for vectors with
+  ;; length only known at runtime.
+
+;; I could randomize the order of foodspots with a different look-fn.
+(defn make-look-fn
+  [env ^double perc-radius]
+  (constantly
+    (hf/double-array
+      (conj [perc-radius (+ 1.0 (* 2.0 (count env)))] ; second element is index of last coordinate
+            (apply concat env)))))
+
+
+;; Note that look-fn plays a different role here than in walks/find-in-seg, as it must.
+;; Version using new ham-fisted typed destructuring macros
+(defn find-in-seg
+  "Only returns the first foodspot found.  The search in order that
+  foodspots are returned by look-fn."
+  [look-fn _ x0 y0 x1 y1]
+  (hfl/let [[perc-radius last-index target-coords] (dbls (look-fn))]
+    (println (class target-coords))
+    (let [near-pt-fn (partial um/near-pt-on-seg x0 y0 x1 y1)] ; Is this a good idea?
+    (loop [i 0]
+      (hfl/let [j (inc i)
+                p (hf/dnth target-coords i)
+                q (hf/dnth target-coords j)
+                [near-x near-y] (near-pt-fn p q)
+                distance (um/distance-2D* near-x near-y p q)]
+        (cond (<= distance perc-radius) [[[p q]] [near-x near-y]] ; seq of single foodspot found, where found from
+              (= j last-index) nil
+              :else (recur (inc i))))))))
+)
 
